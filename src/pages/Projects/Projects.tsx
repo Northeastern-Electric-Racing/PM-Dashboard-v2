0e24768ae8220733d868a1847517ff13a4fbd8f4
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-02-02 01:29:45 -0800

    #47 - Added useEffect example

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,9 +1,11 @@
+import ProjectsTable from '../../containers/ProjectsTable/ProjectsTable';
 import './Projects.module.css';
 
 const Projects: React.FC = () => {
   return (
     <div>
       <h1>This is the Projects Page</h1>
+      <ProjectsTable />
     </div>
   );
 };

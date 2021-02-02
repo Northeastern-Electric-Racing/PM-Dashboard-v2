e2baa08f24231fe0fff4210dd810de329f477fc0
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-02-02 01:54:25 -0800

    #47 - Fix CSS errors, add named import variant

--- src/pages/change-requests/change-requests.tsx
+++ src/pages/change-requests/change-requests.tsx
@@ -1,10 +1,10 @@
-import './change-requests.module.css';
+import styles from './change-requests.module.css';
 import ChangeRequestsTable from '../../containers/change-requests-table/change-requests-table';
 
 const ChangeRequests: React.FC = () => {
   return (
     <div>
-      <h1>This is the Change Requests Page</h1>
+      <h1 className={styles.title}>This is the Change Requests Page</h1>
       <ChangeRequestsTable />
     </div>
   );

97725db262cc2c6b21be0a1defcbcbd7b5deb057
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-02-02 00:46:58 -0800

    #47 - Add state to change request container

--- src/containers/ChangeRequestsTable/ChangeRequestsTable.tsx
+++ src/containers/ChangeRequestsTable/ChangeRequestsTable.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './ChangeRequestsTable.module.css';
 
 function ChangeRequestsTable() {
+  const crOptions: Array<String> = ['Delay', 'Budget', 'Scope'];
+  const [changeRequest, setChangeRequest] = useState(0);
+
+  const switchCR = () => {
+    changeRequest === 2 ? setChangeRequest(0) : setChangeRequest(changeRequest + 1);
+  };
+
   return (
     <div>
       <h1>This is the Change Requests Table container</h1>
+      <p>{crOptions[changeRequest]}</p>
+      <a onClick={switchCR}>Click me!</a>
     </div>
   );
 }

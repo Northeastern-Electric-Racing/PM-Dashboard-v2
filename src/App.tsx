7eeb2f08cbc123039111622c92a08fdb234d6d7f
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-02-08 00:50:53 -0800

    #67 - Gut the template app to allow room for prototyping

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ChangeRequests from './pages/change-requests/change-requests';
 import Projects from './pages/projects/projects';
@@ -7,21 +6,6 @@ import Projects from './pages/projects/projects';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>SCROLL DOWN</p>
-      </header>
       <div>
         <Projects />
         <ChangeRequests />

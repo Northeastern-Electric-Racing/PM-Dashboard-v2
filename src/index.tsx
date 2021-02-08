db7b88abfba22715353f9d453371ae1a8c18c30c
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-02-07 17:31:34 -0800

    #67 - Installed react-bootstrap-table2 and CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

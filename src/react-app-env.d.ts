156d9b921b09c063ad684977d161383d83ddd094
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-02-23 23:41:37 -0800

    #76 - Add license snippets

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
 /// <reference types="react-scripts" />
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,8 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)

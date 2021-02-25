0437043fe2712ee5722c5836cb13ee3f57dc36f6
albeartoz <zhang.alb@northeastern.edu>
2021-02-24 19:25:33 -0500

    add styling

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
+import 'react-pro-sidebar/dist/css/styles.css'
 import './sidebar.module.css';
-import { MenuItem, ProSidebar, SidebarHeader, SidebarContent } from 'react-pro-sidebar';
-
+import { MenuItem, ProSidebar, Menu, SidebarHeader, SidebarContent } from 'react-pro-sidebar';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <ProSidebar
+        <div className="layout">
+      <aside>
+        <ProSidebar collapsed={collapsed}>
+          <Menu iconShape="square">
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 3</MenuItem>
+          </Menu>
+        </ProSidebar>
+      </aside>
+      <main className="content">
+        <div>
+          <a href="!#" className="btn" onClick={() => setCollapsed(!collapsed)}>
+            Collapse
+          </a>
+        </div>
+      </main>
+    </div>
+
+        /* <div className="layout">
+            <ProSidebar
             toggled={true}
         >
             <SidebarHeader>
@@ -18,7 +40,7 @@ const Sidebar = () => {
                     Home
                 </MenuItem>
                 <MenuItem>
-                        Gantt
+                    Gantt
                 </MenuItem>
                 <MenuItem>
                     Projects
@@ -35,6 +57,7 @@ const Sidebar = () => {
             </SidebarContent>
             
         </ProSidebar> 
+        </div> */
     )
 }
 

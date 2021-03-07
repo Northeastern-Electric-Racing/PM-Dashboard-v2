589514cf6b9d5544a950f82257f133757ab16d62
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-03-06 21:03:32 -0800

    #98 - Build example data

--- src/utils/src/index.ts
+++ src/utils/src/index.ts
@@ -4,5 +4,10 @@
  */
 
 export * from './example-shared';
+export * from './dummy-data';
+
 export * from './types/change-request-types';
+export * from './types/implemented-change-types';
 export * from './types/project-types';
+export * from './types/user-types';
+export * from './types/wbs-types';

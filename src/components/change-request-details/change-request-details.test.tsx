434b7642874b8ebf25678edd6ed5e947dfbf9d81
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-03-06 22:10:31 -0800

    #90 - Built basic change request details component

--- src/components/change-request-details/change-request-details.test.tsx
+++ src/components/change-request-details/change-request-details.test.tsx
@@ -4,12 +4,11 @@
  */
 
 import { render, screen } from '@testing-library/react';
-import { ChangeRequest } from 'utils';
+import { exampleStandardChangeRequest } from 'utils';
 import ChangeRequestDetails from './change-request-details';
 
 test('Renders title', () => {
-  const exChangeRequest: ChangeRequest = { submitter: 'Matt McCauley', type: 'Delay' };
-  render(<ChangeRequestDetails changeRequest={exChangeRequest} />);
-  const titleElement = screen.getByText(/Change Request Details/i);
+  render(<ChangeRequestDetails changeRequest={exampleStandardChangeRequest} />);
+  const titleElement = screen.getByText(/Change Request/i);
   expect(titleElement).toBeInTheDocument();
 });

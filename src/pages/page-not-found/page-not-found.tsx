29b6c63aac4b7a269ee67c15e853fb94974e4cc1
Jeff <Airlegoland@gmail.com>
2021-03-15 01:49:18 -0400

    #89 Restructured 404 page files to fit project specifications, minor CSS changes

---
+++ src/pages/page-not-found/page-not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './page-not-found.module.css';
+
+export const PageNotFound: React.FC = () => {
+  return (
+    <body>
+      <div className={styles.page_not_found}>
+        <h1 className={styles.h1}>404</h1>
+        <h3 className={styles.h3}>Page not found</h3>
+      </div>
+    </body>
+  );
+};

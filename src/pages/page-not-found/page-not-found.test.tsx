6ee234c982e2fa39191f5e6d321522cdb20d0bda
Jeff <Airlegoland@gmail.com>
2021-03-15 02:06:55 -0400

    #89 added test to page-not-found

--- src/pages/page-not-found/page-not-found.test.tsx
+++ src/pages/page-not-found/page-not-found.test.tsx
@@ -1,1 +1,8 @@
-import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { PageNotFound } from './page-not-found';
+
+test('404 rendered', () => {
+  render(<PageNotFound />);
+  const title = screen.getByText(/404/i);
+  expect(title).toBeInTheDocument();
+});

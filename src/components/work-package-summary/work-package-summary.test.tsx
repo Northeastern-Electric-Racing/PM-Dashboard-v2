6f0f4c13a3175dd67ab35528d6798999f342221c
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2021-03-31 23:31:09 -0400

    #91 - Starting testing

---
+++ src/components/work-package-summary/work-package-summary.test.tsx
@@ -0,0 +1,12 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import { render, screen } from '@testing-library/react';
+import WorkPackageSummary from './work-package-summary';
+import {
+    exampleWorkPackage1,
+    exampleWorkPackage2,
+    exampleWorkPackage3
+} from 'utils';

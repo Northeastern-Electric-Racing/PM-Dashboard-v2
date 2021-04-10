8811ce5fa4c991435dfc882156b487b1a065a4d8
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2021-04-09 22:03:07 -0400

    #91 - Added tests for end date and list formatting

--- src/components/work-package-summary/work-package-summary.test.tsx
+++ src/components/work-package-summary/work-package-summary.test.tsx
@@ -5,12 +5,52 @@
 
 import { render, screen } from '@testing-library/react';
 import WorkPackageSummary from './work-package-summary';
+import { 
+    formatList,
+    formatEndDate } from './work-package-summary';
 import {
     exampleWorkPackage1,
     exampleWorkPackage2,
     exampleWorkPackage3
 } from 'utils';
+import { wbsPipe } from '../../shared/pipes';
 
-test('Renders title', () => {
-    render(<WorkPackageSummary workPackage={exampleWorkPackage1} />);
-  });
+describe('Formatting lists tests', () => {
+    test('Formatting Wbs Numbers', () => {
+        expect(formatList(exampleWorkPackage1.dependencies, wbsPipe)).toBe("");
+        expect(formatList(exampleWorkPackage2.dependencies, wbsPipe)).toBe("1.1.1");
+        expect(formatList(exampleWorkPackage3.dependencies, wbsPipe)).toBe("1.12.0, 2.5.0");
+    });
+
+    test('Formatting Rules', () => {
+        expect(formatList(exampleWorkPackage1.rules, (str: string): string => { return str; })).toBe("EV3.5.2");
+        expect(formatList(exampleWorkPackage2.rules, (str: string): string => { return str; })).toBe("T12.3.2, T8.2.6");
+        expect(formatList(exampleWorkPackage3.rules, (str: string): string => { return str; })).toBe("EV1.4.7, EV6.3.10");
+    });
+
+    test('for other case', () => {
+        expect(formatList([1, 2, 3, 4], (num: number): string => { return "" + num; })).toBe("1, 2, 3, 4");
+        expect(formatList([], (num: number): string => { return "" + num; })).toBe("");
+        expect(formatList([1], (num: number): string => { return "" + num; })).toBe("1");
+    });
+});
+
+describe('Formatting end date tests', () => {
+    test('with dummy data', () => {
+        expect(formatEndDate(exampleWorkPackage1.startDate, exampleWorkPackage1.duration)).toBe("1/22/2021");
+        expect(formatEndDate(exampleWorkPackage2.startDate, exampleWorkPackage2.duration)).toBe("2/26/2021");
+        expect(formatEndDate(exampleWorkPackage3.startDate, exampleWorkPackage3.duration)).toBe("1/15/2021");
+    });
+
+    test('with edge dates', () => {
+        expect(formatEndDate(new Date("12/25/20"), 3)).toBe("1/15/2021");
+        expect(formatEndDate(new Date("1/3/21"), 3)).toBe("1/24/2021");
+        expect(formatEndDate(new Date("3/1/21"), 10)).toBe("5/10/2021");
+    });
+});
+
+describe('Rendering Work Packagae Summary Test', () => {
+    test('Renders title', () => {
+        render(<WorkPackageSummary workPackage={exampleWorkPackage1} />);
+    });
+});
--- src/components/work-package-summary/work-package-summary.tsx
+++ src/components/work-package-summary/work-package-summary.tsx
@@ -16,41 +16,24 @@ interface WorkPackageSummaryProps {
   workPackage: WorkPackage;
 }
 
-// Formats the dependencies to be displayed as a list
-const formatDependencies: Function = (dep: WbsNumber[]): string => {
-  var i = 0;
-  var str = "";
-  if (dep.length === 0) {
-    return str;
-  }
-  for (i = 0; i < dep.length - 1; i++) {
-    str = str  + wbsPipe(dep[i]) + ", ";
-  }
-  return str + wbsPipe(dep[i]);
-};
-
-// Formats the rules to be displayed as a list
-const formatRules: Function = (rules: string[]): string => {
+// Formats an array of objects into a string that is a list
+export const formatList: Function = <T, >(rules: T[], app: (el: T) => string): string => {
   var i = 0;
   var str = "";
   if (rules.length === 0) {
     return str;
   }
   for (i = 0; i < rules.length - 1; i++) {
-    str = str  + rules[i] + ", ";
+    str = str  + app(rules[i])+ ", ";
   }
-  return str + rules[i];
+  return str + app(rules[i]);
 };
 
 // Formats the end date as a string
-const formatEndDate: Function = (startDate: Date, dur: number): string => {
-  return findEndDate(startDate, dur).toLocaleDateString();
-};
-
-const findEndDate: Function = (startDate: Date, dur: number): Date => {
+export const formatEndDate: Function = (startDate: Date, dur: number): string => {
   var endDate = new Date(startDate);
   endDate.setDate(endDate.getDate() + dur*7)
-  return endDate;
+  return endDate.toLocaleDateString();
 };
 
 const WorkPackageSummary: React.FC<WorkPackageSummaryProps> = ({
@@ -74,8 +57,8 @@ const WorkPackageSummary: React.FC<WorkPackageSummaryProps> = ({
             <Card.Body>
               <p>{workPackage.deliverable}</p>
               <div className={styles.halfDiv}>
-                <p><b>Dependencies:</b> {formatDependencies(workPackage.dependencies)}</p>
-                <p><b>Rules:</b> {formatRules(workPackage.rules)}</p>
+                <p><b>Dependencies:</b> {formatList(workPackage.dependencies, wbsPipe)}</p>
+                <p><b>Rules:</b> {formatList(workPackage.rules, (str: string): string => { return str; })}</p>
                 <p><b>Budget:</b> {dollarsPipe(workPackage.budget)}</p>
               </div>
               <div className={styles.halfDiv}>

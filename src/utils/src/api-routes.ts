1c1f76fe1fc245940a85ab793f8c82f7feaceab5
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-04-11 14:57:59 -0700

    #111 - Clarify naming and fix error

--- src/utils/src/api-routes.ts
+++ src/utils/src/api-routes.ts
@@ -9,28 +9,28 @@ const USERS_LOGIN: string = `${USERS}/login`;
 
 /**************** Projects Endpoint ****************/
 const PROJECTS: string = `/projects`;
-const PROJECTS_SINGLE: string = `${PROJECTS}/:wbs`;
+const PROJECTS_BY_WBS: string = `${PROJECTS}/:wbs`;
 
 /**************** Work Packages Endpoint ****************/
 const WORK_PACKAGES: string = `/work-packages`;
-const WORK_PACKAGES_SINGLE: string = `${WORK_PACKAGES}/:wbs`;
+const WORK_PACKAGES_BY_WBS: string = `${WORK_PACKAGES}/:wbs`;
 
 /**************** Change Requests Endpoint ****************/
-const CHANGE_REQUESTS: string = `/projects`;
-const CHANGE_REQUESTS_SINGLE: string = `${CHANGE_REQUESTS}/:wbs`;
-const CHANGE_REQUESTS_SINGLE_REVIEW: string = `${CHANGE_REQUESTS_SINGLE}\\:review`;
+const CHANGE_REQUESTS: string = `/change-requests`;
+const CHANGE_REQUESTS_BY_ID: string = `${CHANGE_REQUESTS}/:id`;
+const CHANGE_REQUESTS_REVIEW: string = `${CHANGE_REQUESTS_BY_ID}\\:review`;
 
 export const apiRoutes = {
   USERS,
   USERS_LOGIN,
 
   PROJECTS,
-  PROJECTS_SINGLE,
+  PROJECTS_BY_WBS,
 
   WORK_PACKAGES,
-  WORK_PACKAGES_SINGLE,
+  WORK_PACKAGES_BY_WBS,
 
   CHANGE_REQUESTS,
-  CHANGE_REQUESTS_SINGLE,
-  CHANGE_REQUESTS_SINGLE_REVIEW
+  CHANGE_REQUESTS_BY_ID,
+  CHANGE_REQUESTS_REVIEW
 };

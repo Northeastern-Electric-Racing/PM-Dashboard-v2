536843a493f1b326de51d862a35a1ab31a785710
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-04-12 23:08:51 -0700

    #59 - Integrate top nav bar into app

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,29 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ChangeRequests from './pages/change-requests/change-requests';
 import Projects from './pages/projects/projects';
-import './App.css';
 import { PageNotFound } from './pages/page-not-found/page-not-found';
 import Login from './pages/login/login';
+import NavTopBar from './components/nav-top-bar/nav-top-bar';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="/projects">
-          Projects
-        </Link>
-        <Link className="nav-link" to="/change-requests">
-          Change Requests
-        </Link>
-      </nav>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/change-requests" component={ChangeRequests} />
-        <Route exact path="/">
-          <h3>Home!</h3>
-        </Route>
-        <Route path="*" component={PageNotFound} />
-      </Switch>
+      <NavTopBar />
+      <div className="page-content">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/change-requests" component={ChangeRequests} />
+          <Route exact path="/">
+            <h3>Home!</h3>
+          </Route>
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }

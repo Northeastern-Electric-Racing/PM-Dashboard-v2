974db90d4f9fbb9db4b9da91db1a464f4bfec2a7
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-04-15 21:48:46 -0700

    #69 - Wire up change requests table container with api fetch and display via view component

--- src/containers/change-requests-table/change-requests-table.test.tsx
+++ src/containers/change-requests-table/change-requests-table.test.tsx
@@ -3,11 +3,49 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { render, screen } from '@testing-library/react';
+import { exampleAllChangeRequests } from 'utils';
 import ChangeRequestsTable from './change-requests-table';
 
-test('Renders title', () => {
+const endpointURL: string = '/.netlify/functions/change-requests';
+
+// Mock the server endpoint(s) that the component will hit
+const server = setupServer(
+  rest.get(endpointURL, (req, res, ctx) => {
+    return res(ctx.json(exampleAllChangeRequests));
+  })
+);
+
+// Sets up the component under test with the desired values and renders it.
+const renderComponent: () => void = () => {
   render(<ChangeRequestsTable />);
-  const titleElement = screen.getByText(/Change Requests Table/i);
-  expect(titleElement).toBeInTheDocument();
+};
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('change requests table container', () => {
+  it('renders the table headers', async () => {
+    renderComponent();
+    expect(screen.getByText(/ID/i)).toBeInTheDocument();
+    expect(screen.getByText(/Submitter/i)).toBeInTheDocument();
+    expect(screen.getByText(/WBS #/i)).toBeInTheDocument();
+    expect(screen.getByText(/Type/i)).toBeInTheDocument();
+    expect(screen.getByText(/Accepted/i)).toBeInTheDocument();
+  });
+
+  it('handles the api throwing an error', async () => {
+    server.use(
+      rest.get(endpointURL, (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+
+    renderComponent();
+
+    expect(screen.getByText(/No Change Requests to Display/i)).toBeInTheDocument();
+  });
 });

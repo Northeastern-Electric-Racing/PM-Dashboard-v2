02e2ced08f31604f7329860b02dbfd50b947f16a
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-04-15 23:50:23 -0700

    #140 - Wire change requests table and single cr page

--- src/pages/change-requests/change-requests.tsx
+++ src/pages/change-requests/change-requests.tsx
@@ -3,14 +3,19 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import styles from './change-requests.module.css';
+import { Route, Switch } from 'react-router-dom';
 import ChangeRequestsTable from '../../containers/change-requests-table/change-requests-table';
+import ChangeRequestDetails from '../change-request-details/change-request-details';
+import styles from './change-requests.module.css';
 
 const ChangeRequests: React.FC = () => {
   return (
     <div>
       <h1 className={styles.title}>This is the Change Requests Page</h1>
-      <ChangeRequestsTable />
+      <Switch>
+        <Route path="/change-requests/:id" component={ChangeRequestDetails} />
+        <Route path="/change-requests" component={ChangeRequestsTable} />
+      </Switch>
     </div>
   );
 };

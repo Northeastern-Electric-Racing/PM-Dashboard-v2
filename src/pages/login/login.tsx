a5a61c1049e60c44178449bba945481d4f610bbf
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-04-16 21:42:58 -0700

    #125 - Reconfig login component with user context and form input group for user's name

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,23 +3,52 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { useHistory } from 'react-router';
+import { Form, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { UserLogInContext } from '../../app/app-main/app-main';
 import styles from './login.module.css';
 
 /**
  * Page for unauthenticated users to do login.
  */
 const Login: React.FC = () => {
+  const history = useHistory();
+  const loginFunc = useContext(UserLogInContext);
+  const [userName, setUserName] = useState<string>('');
+
+  const updateName = (event: any) => setUserName(event.target.value);
+
+  const formSubmit = (event: any) => {
+    event.preventDefault();
+    loginFunc(userName);
+    history.push('/');
+  };
+
   return (
     <div className={`card mx-auto mt-sm-5 ${styles.card}`}>
       <div className="card-body">
         <h5 className="card-title">NER PM Dashboard</h5>
-        <p className="card-text">Login Required</p>
-        <div className={styles.button}>
-          <Link to="/">
-            <button className="btn btn-primary">Login</button>
-          </Link>
-        </div>
+        <p id="login-text" className="card-text">
+          Login Required
+        </p>
+        <Form onSubmit={formSubmit}>
+          <InputGroup>
+            <FormControl
+              value={userName}
+              onChange={updateName}
+              placeholder="Name"
+              aria-label="name"
+              aria-describedby="login-text"
+            />
+            <InputGroup.Append>
+              <Button variant="primary" type="submit">
+                Log In
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
       </div>
     </div>
   );

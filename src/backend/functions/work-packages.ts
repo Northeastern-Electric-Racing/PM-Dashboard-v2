a99641a9317d7306cd199393edaa4cf4366b29ce
TheFishToldMeSo <phantranh2206@gmail.com>
2021-04-18 12:58:53 +0700

    #71 - creating /work-packages endpoint

---
+++ src/backend/functions/work-packages.ts
@@ -0,0 +1,20 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import { Context } from 'aws-lambda';
+import { exampleAllWorkPackages } from 'utils';
+
+export async function handler(event: any, context: Context) {
+  try {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(exampleAllWorkPackages)
+    };
+  } catch (error) {
+    console.error(error);
+    return { statusCode: 500 };
+  }
+}

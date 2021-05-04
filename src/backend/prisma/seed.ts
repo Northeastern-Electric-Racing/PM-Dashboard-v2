e4c3c5a36169c7d875fb99a7c9b66ec848750cd1
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-05-04 09:45:13 -0700

    #185 - Created seed-specifc data and update the seed file to utilize seed data

--- src/backend/prisma/seed.ts
+++ src/backend/prisma/seed.ts
@@ -3,92 +3,89 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { PrismaClient, CR_Type, Scope_CR_Why_Type, WBS_Element_Status } from '@prisma/client';
-import {
-  ChangeRequestReason,
-  exampleAllUsers,
-  exampleProject1,
-  exampleStandardChangeRequest
-} from 'utils';
+import { PrismaClient } from '@prisma/client';
+import { dbSeedAllUsers } from './seed-data/users';
+import { dbSeedAllProjects } from './seed-data/projects';
+import { dbSeedAllWorkPackages } from './seed-data/work-packages';
+import { dbSeedAllChangeRequests } from './seed-data/change-requests';
 
 const prisma = new PrismaClient();
 
-const reasonWhyTypeMap: Map<ChangeRequestReason, Scope_CR_Why_Type> = new Map();
-reasonWhyTypeMap.set(ChangeRequestReason.Estimation, Scope_CR_Why_Type.ESTIMATION);
-reasonWhyTypeMap.set(ChangeRequestReason.Manufacturing, Scope_CR_Why_Type.MANUFACTURING);
-reasonWhyTypeMap.set(ChangeRequestReason.School, Scope_CR_Why_Type.SCHOOL);
-reasonWhyTypeMap.set(ChangeRequestReason.Rules, Scope_CR_Why_Type.RULES);
-reasonWhyTypeMap.set(ChangeRequestReason.OtherProject, Scope_CR_Why_Type.OTHER_PROJECT);
-reasonWhyTypeMap.set(ChangeRequestReason.Other, Scope_CR_Why_Type.OTHER);
+// const reasonWhyTypeMap: Map<ChangeRequestReason, Scope_CR_Why_Type> = new Map();
+// reasonWhyTypeMap.set(ChangeRequestReason.Estimation, Scope_CR_Why_Type.ESTIMATION);
+// reasonWhyTypeMap.set(ChangeRequestReason.Manufacturing, Scope_CR_Why_Type.MANUFACTURING);
+// reasonWhyTypeMap.set(ChangeRequestReason.School, Scope_CR_Why_Type.SCHOOL);
+// reasonWhyTypeMap.set(ChangeRequestReason.Rules, Scope_CR_Why_Type.RULES);
+// reasonWhyTypeMap.set(ChangeRequestReason.OtherProject, Scope_CR_Why_Type.OTHER_PROJECT);
+// reasonWhyTypeMap.set(ChangeRequestReason.Other, Scope_CR_Why_Type.OTHER);
 
-const reasonToDbWhyType: (reason: ChangeRequestReason) => Scope_CR_Why_Type = (reason) => {
-  if (reasonWhyTypeMap.get(reason) === undefined) {
-    throw new Error('reason not found in map');
-  }
-  return reasonWhyTypeMap.get(reason)!;
-};
+// const reasonToDbWhyType: (reason: ChangeRequestReason) => Scope_CR_Why_Type = (reason) => {
+//   if (reasonWhyTypeMap.get(reason) === undefined) {
+//     throw new Error('reason not found in map');
+//   }
+//   return reasonWhyTypeMap.get(reason)!;
+// };
 
 const performSeed: () => Promise<void> = async () => {
-  for (const exampleUser of exampleAllUsers) {
-    await prisma.user.create({
+  for (const seedUser of dbSeedAllUsers) {
+    await prisma.user.create({ data: { ...seedUser } });
+  }
+
+  for (const seedProject of dbSeedAllProjects) {
+    await prisma.project.create({
       data: {
-        userId: exampleUser.id,
-        firstName: exampleUser.firstName,
-        lastName: exampleUser.lastName,
-        emailId: exampleUser.emailId,
-        lastLogin: exampleUser.lastLogin
+        wbsElement: { create: { ...seedProject.wbsElementFields } },
+        ...seedProject.projectFields
       }
     });
   }
 
-  await prisma.project.create({
-    data: {
-      wbsElement: {
-        create: {
-          carNumber: exampleProject1.wbsNum.car,
-          projectNumber: exampleProject1.wbsNum.project,
-          workPackageNumber: exampleProject1.wbsNum.workPackage,
-          name: exampleProject1.name,
-          status: WBS_Element_Status.ACTIVE,
-          projectLeadId: exampleProject1.projectLead.id,
-          projectManagerId: exampleProject1.projectManager.id
-        }
-      },
-      googleDriveFolderLink: exampleProject1.gDriveLink,
-      slideDeckLink: exampleProject1.slideDeckLink,
-      bomLink: exampleProject1.bomLink,
-      taskListLink: exampleProject1.taskListLink
-    }
-  });
-
-  await prisma.change_Request.create({
-    data: {
-      crId: exampleStandardChangeRequest.id,
-      submitterId: exampleStandardChangeRequest.submitter.id,
-      wbsElementId: 1,
-      dateSubmitted: exampleStandardChangeRequest.dateSubmitted,
-      type: CR_Type.DESIGN_ISSUE,
-      dateReviewed: exampleStandardChangeRequest.dateReviewed,
-      accepted: exampleStandardChangeRequest.accepted,
-      reviewNotes: exampleStandardChangeRequest.reviewNotes,
-      scopeChangeRequest: {
-        create: {
-          what: exampleStandardChangeRequest.what,
-          why: {
-            create: exampleStandardChangeRequest.why.map((whyEle) => {
-              return {
-                type: reasonToDbWhyType(whyEle.reason),
-                explain: whyEle.explain
-              };
-            })
-          },
-          scopeImpact: exampleStandardChangeRequest.scopeImpact,
-          timelineImpact: exampleStandardChangeRequest.timelineImpact,
-          budgetImpact: exampleStandardChangeRequest.budgetImpact
-        }
+  for (const seedWorkPackage of dbSeedAllWorkPackages) {
+    await prisma.work_Package.create({
+      data: {
+        wbsElement: { create: { ...seedWorkPackage.wbsElementFields } },
+        project: { connect: { projectId: seedWorkPackage.projectId } },
+        ...seedWorkPackage.workPackageFields,
+        descriptionBullets: { create: seedWorkPackage.descriptionBullets }
       }
+    });
+  }
+
+  for (const seedChangeRequest of dbSeedAllChangeRequests) {
+    const data: any = {
+      submitter: { connect: { userId: seedChangeRequest.submitterId } },
+      wbsElement: { connect: { wbsElementId: seedChangeRequest.wbsElementId } },
+      ...seedChangeRequest.changeRequestFields,
+      changes: { create: seedChangeRequest.changes }
+    };
+    if (seedChangeRequest.scopeChangeRequestFields) {
+      data.scopeChangeRequest = {
+        create: {
+          ...seedChangeRequest.scopeChangeRequestFields.otherFields,
+          why: { create: seedChangeRequest.scopeChangeRequestFields.why }
+        }
+      };
     }
-  });
+    if (seedChangeRequest.activationChangeRequestFields) {
+      data.activationChangeRequest = {
+        create: {
+          ...seedChangeRequest.activationChangeRequestFields.otherFields,
+          projectLead: {
+            connect: { userId: seedChangeRequest.activationChangeRequestFields.projectLeadId }
+          },
+          projectManager: {
+            connect: { userId: seedChangeRequest.activationChangeRequestFields.projectManagerId }
+          }
+        }
+      };
+    }
+    if (seedChangeRequest.stageGateChangeRequestFields) {
+      data.stageGateChangeRequest = {
+        create: { ...seedChangeRequest.stageGateChangeRequestFields }
+      };
+    }
+    await prisma.change_Request.create({ data });
+  }
 };
 
 performSeed()

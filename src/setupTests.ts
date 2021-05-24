4c8b803fb1ae53933c96aa4fd53189485ce37ea3
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-05-24 09:10:42 -0700

    #221 - Mock localStorage for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -8,3 +8,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  key: jest.fn(),
+  length: 0
+};
+
+window.localStorage.__proto__ = localStorageMock;

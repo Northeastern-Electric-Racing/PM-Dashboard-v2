14b9dd0c3b7c3f21fb024a09a3df7d83ff4abe10
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-06-03 21:36:57 -0700

    #235 - Rearrange and refactor, abstract to use buildFailureResponse()

--- src/utils/src/api-utils.ts
+++ src/utils/src/api-utils.ts
@@ -26,36 +26,41 @@ export const buildResponse = (
 };
 
 /**
+ * Builds a success API response object
+ */
+export const buildSuccessResponse = (body: Object) => {
+  return buildResponse(200, body);
+};
+
+/**
+ * Builds a failure API response object
+ */
+export const buildFailureResponse = (status: number, message: string) => {
+  return buildResponse(status, { message });
+};
+
+/**
  * Builds a client error failure API response object
  * @param message Error message
  */
-export const buildClientFailureResponse = (message: string): HandlerResponse => {
-  return buildResponse(400, { message });
-};
+export const buildClientFailureResponse = (message: string) => buildFailureResponse(400, message);
 
 /**
  * Builds a server error failure API response object
  * @param message Error message
  */
-export const buildServerFailureResponse = (message: string): HandlerResponse => {
-  return buildResponse(500, { message });
-};
+export const buildServerFailureResponse = (message: string) => buildFailureResponse(500, message);
 
 /**
  * Builds a not found error API response object
  * @param type The type of item requested
  * @param item The requested item which could not be found
  */
-export const buildNotFoundResponse = (type: string, item: string): HandlerResponse => {
-  return buildResponse(404, { message: `Could not find the requested ${type} [${item}].` });
+export const buildNotFoundResponse = (type: string, item: string) => {
+  return buildFailureResponse(404, `Could not find the requested ${type} [${item}].`);
 };
 
-/**
- * Builds a success API response object
- */
-export const buildSuccessResponse = (body: Object): HandlerResponse => {
-  return buildResponse(200, body);
-};
+// ---------------------------------
 
 /**
  * Finds the matching route and executes the route's function

6cb3c81c34d88479768028cd69404e7e52bf2536
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-06-03 21:28:35 -0700

    #235 - Simplify function signatures

--- src/utils/src/api-utils.ts
+++ src/utils/src/api-utils.ts
@@ -11,13 +11,13 @@ import { ApiRoute } from './types/api-utils-types';
 /**
  * Builds a standard API response object
  */
-export const buildResponse: (
+export const buildResponse = (
   statusCode: number,
   body: Object,
   headers?: {
     [header: string]: boolean | number | string;
   }
-) => HandlerResponse = (statusCode, body, headers) => {
+): HandlerResponse => {
   return {
     statusCode,
     headers,
@@ -29,7 +29,7 @@ export const buildResponse: (
  * Builds a server error failure API response object
  * @param message Error message
  */
-export const buildServerFailureResponse: (message: string) => HandlerResponse = (message) => {
+export const buildServerFailureResponse = (message: string): HandlerResponse => {
   return buildResponse(500, { message });
 };
 
@@ -38,25 +38,25 @@ export const buildServerFailureResponse: (message: string) => HandlerResponse = 
  * @param type The type of item requested
  * @param item The requested item which could not be found
  */
-export const buildNotFoundResponse: (type: string, item: string) => HandlerResponse = (t, i) => {
-  return buildResponse(404, { message: `Could not find the requested ${t} [${i}].` });
+export const buildNotFoundResponse = (type: string, item: string): HandlerResponse => {
+  return buildResponse(404, { message: `Could not find the requested ${type} [${item}].` });
 };
 
 /**
  * Builds a success API response object
  */
-export const buildSuccessResponse: (body: Object) => HandlerResponse = (body) => {
+export const buildSuccessResponse = (body: Object): HandlerResponse => {
   return buildResponse(200, body);
 };
 
 /**
  * Finds the matching route and executes the route's function
  */
-export const routeMatcher: (
+export const routeMatcher = (
   routes: ApiRoute[],
   event: HandlerEvent,
   context: HandlerContext
-) => HandlerResponse = (routes, event, context) => {
+): HandlerResponse => {
   for (let index = 0; index < routes.length; index += 1) {
     const singleRoute: ApiRoute = routes[index];
     const requestPath: string = !event.path.startsWith(API_URL) ? API_URL + event.path : event.path;

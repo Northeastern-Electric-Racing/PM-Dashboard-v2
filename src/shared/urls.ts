eee1bfff8cfc0f94903d7d5766fa0bd22c94a720
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-06-13 02:01:09 -0700

    #248 - Created API URLs

---
+++ src/shared/urls.ts
@@ -0,0 +1,44 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+/**
+ * This file centralizes URLs used to query the API.
+ */
+
+const API_URL: string = `/.netlify/functions`;
+
+/**************** Users Endpoint ****************/
+const users = () => `${API_URL}/users`;
+const usersById = (id: string) => `${users()}/${id}`;
+const usersLogin = () => `${users()}/auth:login`;
+
+/**************** Projects Endpoint ****************/
+const projects = () => `${API_URL}/projects`;
+const projectsByWbsNum = (wbsNum: string) => `${projects()}/${wbsNum}`;
+
+/**************** Work Packages Endpoint ****************/
+const workPackages = () => `${API_URL}/work-packages`;
+const workPackagesByWbsNum = (wbsNum: string) => `${workPackages()}/${wbsNum}`;
+
+/**************** Change Requests Endpoint ****************/
+const changeRequests = () => `${API_URL}/change-requests`;
+const changeRequestsById = (id: string) => `${changeRequests()}/${id}`;
+const changeRequestsReview = (id: string) => `${changeRequestsById(id)}/process:review`;
+
+export const apiUrls = {
+  users,
+  usersById,
+  usersLogin,
+
+  projects,
+  projectsByWbsNum,
+
+  workPackages,
+  workPackagesByWbsNum,
+
+  changeRequests,
+  changeRequestsById,
+  changeRequestsReview
+};

fed1f97672a4147585f0141f890ff1570f174c77
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-06-16 23:20:10 -0700

    #201 - Create and wire up settings page

--- src/components/app/app-authenticated/app-authenticated.tsx
+++ src/components/app/app-authenticated/app-authenticated.tsx
@@ -9,6 +9,7 @@ import Projects from '../../projects/projects';
 import { PageNotFound } from '../../whole-app/page-not-found/page-not-found';
 import Home from '../../home/home';
 import NavTopBar from '../../whole-app/nav-top-bar/nav-top-bar';
+import Settings from '../../settings/settings';
 import styles from './app-authenticated.module.css';
 
 const AppAuthenticated: React.FC = () => {
@@ -19,6 +20,7 @@ const AppAuthenticated: React.FC = () => {
         <Switch>
           <Route path="/projects" component={Projects} />
           <Route path="/change-requests" component={ChangeRequests} />
+          <Route path="/settings" component={Settings} />
           <Route exact path="/" component={Home} />
           <Route path="*" component={PageNotFound} />
         </Switch>

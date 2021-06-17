b4104eacb77c02973aa6c222bf66e83cd46a8a1a
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-06-16 23:18:14 -0700

    #201 - Refactor routes file

--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -4,18 +4,18 @@
  */
 
 /**************** General Section ****************/
-const HOME: string = `/`;
-const LOGIN: string = `/login`;
-const SETTINGS: string = `/settings`;
+const HOME = `/`;
+const LOGIN = `/login`;
+const SETTINGS = `/settings`;
 
 /**************** Projects Section ****************/
-const PROJECTS: string = `/projects`;
-const PROJECTS_BY_WBS: string = `${PROJECTS}/:wbsNum`;
+const PROJECTS = `/projects`;
+const PROJECTS_BY_WBS = PROJECTS + `/:wbsNum`;
 
 /**************** Change Requests Section ****************/
-const CHANGE_REQUESTS: string = `/change-requests`;
-const CHANGE_REQUESTS_BY_ID: string = `${CHANGE_REQUESTS}/:id`;
-const CHANGE_REQUESTS_NEW: string = `${CHANGE_REQUESTS}/new`;
+const CHANGE_REQUESTS = `/change-requests`;
+const CHANGE_REQUESTS_BY_ID = CHANGE_REQUESTS + `/:id`;
+const CHANGE_REQUESTS_NEW = CHANGE_REQUESTS + `/new`;
 
 export const routes = {
   HOME,

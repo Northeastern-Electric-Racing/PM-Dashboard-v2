1fbe46981ae4bbc1c9031f5071d654a5609c4497
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-06-17 00:04:35 -0700

    #206 - Remove className prop

--- src/components/shared/page-title/page-title.tsx
+++ src/components/shared/page-title/page-title.tsx
@@ -7,12 +7,11 @@ import './page-title.module.css';
 
 interface PageTitleProps {
   title: string;
-  className?: string;
 }
 
 // Common component for all page titles
-const PageTitle: React.FC<PageTitleProps> = ({ title, className }) => {
-  return <h3 className={'mx-5 pt-2 pb-1 ' + className}>{title}</h3>;
+const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
+  return <h3 className={'mx-5 pt-2 pb-1'}>{title}</h3>;
 };
 
 export default PageTitle;

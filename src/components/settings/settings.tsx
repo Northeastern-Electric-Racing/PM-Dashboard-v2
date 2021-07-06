f15bcdd0080ab1059aa43b60bd03e88061889be5
leoRysing <leshaigor@gmail.com>
2021-07-06 13:19:03 -0400

    rough update to settings page, using components
    I am currently looking into the correct way to use the PageBlock component

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -7,12 +7,21 @@ import { useContext } from 'react';
 import { UserContext } from '../app/app-context/app-context';
 import './settings.module.css';
 
+import { PageTitle } from '../shared/page-title/page-title';
+import { PageBlock } from '../shared/page-block/page-block';
+
+// †ødø; add the two new components, Page Title and Page Block, from shared
+
 const Settings: React.FC = () => {
   const user = useContext(UserContext);
+  const title = PageTitle("This is the Settings Page");
+  const body = PageBlock("User Settings", "", "User: {user}");
+  // PageBlock({title: "User Settings", headerRight: "", User: {user}})
+  // <p>User: {user}</p>
   return (
     <>
-      <h3>This is the Settings Page</h3>
-      <p>User: {user}</p>
+      title
+      body
     </>
   );
 };

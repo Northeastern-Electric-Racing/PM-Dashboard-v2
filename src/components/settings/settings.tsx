f0ee9268879da05b230f3974af7c283ea3dbad53
leoRysing <leshaigor@gmail.com>
2021-07-26 22:14:10 -0400

    updates to settings doc

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -6,23 +6,16 @@
 import { useContext } from 'react';
 import { UserContext } from '../app/app-context/app-context';
 import './settings.module.css';
-
 import PageTitle from '../shared/page-title/page-title';
 import PageBlock from '../shared/page-block/page-block';
 
-// †ødø; add the two new components, Page Title and Page Block, from shared
-
 const Settings: React.FC = () => {
   const user = useContext(UserContext);
-  const title = <PageTitle title="This is the Settings Page" />
-  // const body = PageBlock("User Settings", "", "User: {user}");
-  // render(<PageBlock title={'test'} headerRight={<>hi</>} body={<>hello</>} />);
   const pageBlockBody = <>User: {user}</>;
-  const body = <PageBlock title="User Settings" headerRight={<></>} body={pageBlockBody} />
   return (
     <>
-      {title}
-      {body}
+      <PageTitle title="This is the Settings Page" />
+      <PageBlock title="User Settings" headerRight={<></>} body={pageBlockBody} />
     </>
   );
 };

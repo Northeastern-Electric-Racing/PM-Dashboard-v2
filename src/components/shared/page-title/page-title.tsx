034eb50d1507f2f006770ffd7e63763f607a0b09
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-14 01:54:46 -0400

    #196 - Enable page title to support right side action button

--- src/components/shared/page-title/page-title.tsx
+++ src/components/shared/page-title/page-title.tsx
@@ -3,15 +3,21 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import './page-title.module.css';
+import styles from './page-title.module.css';
 
 interface PageTitleProps {
   title: string;
+  actionButton?: JSX.Element;
 }
 
 // Common component for all page titles
-const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
-  return <h3 className={'mx-5 pt-3 pb-1'}>{title}</h3>;
+const PageTitle: React.FC<PageTitleProps> = ({ title, actionButton }) => {
+  return (
+    <div className={'mx-5 pt-3 pb-1 ' + styles.titleRow}>
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.actionButton}>{actionButton}</div>
+    </div>
+  );
 };
 
 export default PageTitle;

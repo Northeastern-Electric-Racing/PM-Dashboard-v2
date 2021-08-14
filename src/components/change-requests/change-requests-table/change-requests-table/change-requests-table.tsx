7fecd2ffbe964bfbb1a847254e5023f95c64db00
VerySeriousHireableAccount <phantranh2206@gmail.com>
2021-08-15 00:16:49 +0700

    #271 - changing mouse hovering effect for project table into finger

--- src/components/change-requests/change-requests-table/change-requests-table/change-requests-table.tsx
+++ src/components/change-requests/change-requests-table/change-requests-table/change-requests-table.tsx
@@ -59,7 +59,7 @@ const ChangeRequestsTable: React.FC<ChangeRequestsTableProps> = ({
     }
   };
 
-  return (
+  return ( 
     <>
       <PageTitle title={'All Change Requests'} />
       <BootstrapTable

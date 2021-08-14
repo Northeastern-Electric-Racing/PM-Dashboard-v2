6c9964bd26219e5c55ee58226f29061d73614af9
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-13 23:07:22 -0400

    #279 - Resolve app public test failures

--- src/components/app/app-public/app-public.test.tsx
+++ src/components/app/app-public/app-public.test.tsx
@@ -3,12 +3,14 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
+import { User } from 'utils';
 import { render, screen, routerWrapperBuilder } from '../../../test-support/test-utils';
+import { exampleAdminUser } from '../../../test-support/test-data/users.stub';
+import { mockAuth } from '../../../test-support/test-data/test-utils.stub';
 import { useAuth } from '../../../services/auth.hooks';
 import { routes } from '../../../shared/routes';
 import { Auth } from '../../../shared/types';
 import AppPublic from './app-public';
-import { mockAuth } from '../../../test-support/test-data/test-utils.stub';
 
 jest.mock('../app-authenticated/app-authenticated', () => {
   return {
@@ -23,13 +25,12 @@ jest.mock('../../../services/auth.hooks');
 
 const mockedUseAuth = useAuth as jest.Mock<Auth>;
 
-const mockHook = () => {
-  mockedUseAuth.mockReturnValue(mockAuth());
+const mockHook = (user?: User) => {
+  mockedUseAuth.mockReturnValue(mockAuth(user));
 };
 
 // Sets up the component under test with the desired values and renders it
 const renderComponent = (path?: string, route?: string) => {
-  mockHook();
   const RouterWrapper = routerWrapperBuilder({ path, route });
   return render(
     <RouterWrapper>
@@ -43,12 +44,12 @@ describe('app public section', () => {
     renderComponent(routes.LOGIN, routes.LOGIN);
 
     expect(screen.getByText('NER PM Dashboard')).toBeInTheDocument();
-    expect(screen.getByText('Login Required')).toBeInTheDocument();
-    expect(screen.getByText('Log In')).toBeInTheDocument();
-    expect(screen.getByLabelText('name')).toBeInTheDocument();
+    expect(screen.getByText(/Login Required/i)).toBeInTheDocument();
+    expect(screen.getByText('Login')).toBeInTheDocument();
   });
 
   it('renders app authenticated', () => {
+    mockHook(exampleAdminUser);
     renderComponent(routes.PROJECTS, routes.PROJECTS);
 
     expect(screen.getByText('app-authenticated')).toBeInTheDocument();

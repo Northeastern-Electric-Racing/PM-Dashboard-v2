c4f3d79887fc525e063aeee0929ed01831c1f6f8
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-13 23:21:26 -0400

    #279 - Fix missed import renaming

--- src/components/app/app-context/app-context.tsx
+++ src/components/app/app-context/app-context.tsx
@@ -5,7 +5,7 @@
 
 import AppContextAuth from '../app-context-auth/app-context-auth';
 import AppContextQuery from '../app-context-query/app-context-query';
-import './app-core.module.css';
+import './app-context.module.css';
 
 const AppContext: React.FC = (props) => {
   return (

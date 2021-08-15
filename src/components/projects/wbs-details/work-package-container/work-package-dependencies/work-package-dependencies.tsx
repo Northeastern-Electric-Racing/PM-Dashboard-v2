7fa086ee6e112632a611cc40050cb541418df569
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-14 22:52:48 -0400

    #292 - Implemented horizontal list in work package dependencies

--- src/components/projects/wbs-details/work-package-container/work-package-dependencies/work-package-dependencies.tsx
+++ src/components/projects/wbs-details/work-package-container/work-package-dependencies/work-package-dependencies.tsx
@@ -7,20 +7,23 @@ import { Link } from 'react-router-dom';
 import { WorkPackage } from 'utils';
 import { routes } from '../../../../../shared/routes';
 import { wbsPipe } from '../../../../../shared/pipes';
-import PageBlock from '../../../../shared/page-block/page-block';
-import styles from './work-package-dependencies.module.css';
+import HorizontalList from '../../../../shared/horizontal-list/horizontal-list';
+import './work-package-dependencies.module.css';
 
 interface WorkPackageDependenciesProps {
   workPackage: WorkPackage;
 }
 
 const WorkPackageDependencies: React.FC<WorkPackageDependenciesProps> = ({ workPackage }) => {
-  const list = workPackage.dependencies.map((ele) => (
-    <p key={wbsPipe(ele)} className={styles.wbsNum}>
-      <Link to={`${routes.PROJECTS}/${wbsPipe(ele)}`}>{wbsPipe(ele)}</Link>
-    </p>
-  ));
-  return <PageBlock title={'Dependencies'} headerRight={<></>} body={<>{list}</>} />;
+  return (
+    <HorizontalList
+      title={'Dependencies'}
+      headerRight={<></>}
+      items={workPackage.dependencies.map((ele) => (
+        <Link to={`${routes.PROJECTS}/${wbsPipe(ele)}`}>{wbsPipe(ele)}</Link>
+      ))}
+    />
+  );
 };
 
 export default WorkPackageDependencies;

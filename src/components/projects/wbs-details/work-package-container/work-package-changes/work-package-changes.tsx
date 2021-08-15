9b99e3f4db3ad2d1bfddb0ac623b20c71ef93cf0
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-14 22:18:36 -0400

    #291 - Implement bullet list in work package changes

--- src/components/projects/wbs-details/work-package-container/work-package-changes/work-package-changes.tsx
+++ src/components/projects/wbs-details/work-package-container/work-package-changes/work-package-changes.tsx
@@ -6,24 +6,23 @@
 import { Link } from 'react-router-dom';
 import { WorkPackage } from 'utils';
 import { routes } from '../../../../../shared/routes';
-import PageBlock from '../../../../shared/page-block/page-block';
-import styles from './work-package-changes.module.css';
+import BulletList from '../../../../shared/bullet-list/bullet-list';
+import './work-package-changes.module.css';
 
 interface WorkPackageChangesProps {
   workPackage: WorkPackage;
 }
 
 const WorkPackageChanges: React.FC<WorkPackageChangesProps> = ({ workPackage }) => {
-  const list = workPackage.changes.map((ic, idx) => (
-    <li key={idx}>
-      [<Link to={`${routes.CHANGE_REQUESTS}/${ic.crId}`}>#{ic.crId}</Link>] {ic.detail}
-    </li>
-  ));
   return (
-    <PageBlock
+    <BulletList
       title={'Changes'}
       headerRight={<></>}
-      body={<ol className={styles.bulletList}>{list}</ol>}
+      list={workPackage.changes.map((ic) => (
+        <>
+          [<Link to={`${routes.CHANGE_REQUESTS}/${ic.crId}`}>#{ic.crId}</Link>] {ic.detail}
+        </>
+      ))}
     />
   );
 };

1040e0ff4b82c40cf0e7a110f7f835a3c39bbf30
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-14 22:47:17 -0400

    #292 - Implement horizontal list in project rules

--- src/components/projects/wbs-details/project-container/project-rules/project-rules.tsx
+++ src/components/projects/wbs-details/project-container/project-rules/project-rules.tsx
@@ -3,20 +3,23 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import PageBlock from '../../../../shared/page-block/page-block';
-import styles from './project-rules.module.css';
+import HorizontalList from '../../../../shared/horizontal-list/horizontal-list';
+import './project-rules.module.css';
 
 interface RulesProps {
   rules: string[];
 }
 
 const ProjectRules: React.FC<RulesProps> = ({ rules }) => {
-  const list = rules.map((str, idx) => (
-    <p key={idx} className={styles.ruleReferences}>
-      {str}
-    </p>
-  ));
-  return <PageBlock title={'Rules'} headerRight={<></>} body={<>{list}</>} />;
+  return (
+    <HorizontalList
+      title={'Rules'}
+      headerRight={<></>}
+      items={rules.map((r) => (
+        <>{r}</>
+      ))}
+    />
+  );
 };
 
 export default ProjectRules;

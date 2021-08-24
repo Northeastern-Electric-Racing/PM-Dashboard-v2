3ac85a52469cb9aeaa98c26251a2a549361797d1
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-08-24 02:30:25 -0400

    #212 - Build out get all projects endpoint support functions in utils package

--- src/utils/src/index.ts
+++ src/utils/src/index.ts
@@ -8,6 +8,8 @@ export * from './types/project-types';
 export * from './types/user-types';
 export * from './types/api-utils-types';
 
+export * from './backend-supports/all-projects';
+
 export * from './dummy-data';
 export * from './validate-wbs';
 export * from './api-utils';

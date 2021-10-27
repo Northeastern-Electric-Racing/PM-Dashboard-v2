ddf48f9538b94c13e3291da1252943e921f9f687
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2021-10-27 18:57:42 -0400

    #203 - Starting from scratch

--- src/components/change-requests/change-requests-filter/change-requests-filter.tsx
+++ src/components/change-requests/change-requests-filter/change-requests-filter.tsx
@@ -3,104 +3,14 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import React, { ReactElement } from 'react';
-import { Card, Container, Form, FormControlProps } from 'react-bootstrap';
-import styles from './change-requests-filter.module.css';
-
-type FormFieldType = 'select' | 'checkbox';
-
-interface FilterFormField {
-  label: string;
-  type: FormFieldType;
-  values: string[];
-  currentValue: number[];
-}
+import React from 'react';
 
 interface FilterFieldStateProps {
-  filterFields: FilterFormField[];
-  setFilterFields: Function;
+  update: () => void;
 }
 
-const ChangeRequestsFilter: React.FC<FilterFieldStateProps> = ({
-  filterFields,
-  setFilterFields
-}) => {
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>, label: string) => {
-    const index = filterFields.map((field) => field.label as string).indexOf(label);
-    const checked = event.target.checked;
-    const id = event.target.id;
-    if (checked) {
-      filterFields[index].currentValue.push(Number(id));
-    } else {
-      const indexOfId = filterFields[index].currentValue.indexOf(Number(id));
-      filterFields[index].currentValue.splice(indexOfId, 1);
-    }
-    console.log('Changing state');
-    setFilterFields(filterFields);
-  };
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement>, label: string) => {
-    console.log(filterFields);
-    const index = filterFields.map((field) => field.label as string).indexOf(label);
-    const value = event.target.value;
-    filterFields[index].currentValue.length = 0;
-    filterFields[index].currentValue.push(Number(value));
-    console.log('Changing state');
-    setFilterFields(filterFields);
-  };
-
-  const selectFilterOptions = (filterField: FilterFormField): ReactElement => {
-    return (
-      <Form.Control
-        onChange={(e) =>
-          handleSelectChange(e as React.ChangeEvent<HTMLInputElement>, filterField.label)
-        }
-        as="select"
-      >
-        {filterField.values.map((value, key) => (
-          <option value={String(key)}>{value}</option>
-        ))}
-      </Form.Control>
-    );
-  };
-
-  const checkboxFilterOptions = (filterField: FilterFormField): ReactElement => {
-    return (
-      <>
-        {filterField.values.map((value, key) => (
-          <Form.Check
-            id={key.toString()}
-            type="checkbox"
-            onChange={(e) =>
-              handleCheckboxChange(e as React.ChangeEvent<HTMLInputElement>, filterField.label)
-            }
-            label={value}
-          />
-        ))}
-      </>
-    );
-  };
-
-  const filterGroup = (filterField: FilterFormField): ReactElement => {
-    return (
-      <Form.Group>
-        <Form.Label>{filterField.label}</Form.Label>
-        {filterField.type === 'select'
-          ? selectFilterOptions(filterField)
-          : checkboxFilterOptions(filterField)}
-      </Form.Group>
-    );
-  };
-
-  return (
-    <div className={styles.filter}>
-      <h3>Filters</h3>
-      <hr />
-      <div className={styles.filterFields}>
-        {filterFields.map((field: FilterFormField) => filterGroup(field))}
-      </div>
-    </div>
-  );
+const ChangeRequestsFilter: React.FC<FilterFieldStateProps> = ({}) => {
+  return <></>;
 };
 
 export default ChangeRequestsFilter;
--- src/components/change-requests/change-requests-table/change-requests-table.tsx
+++ src/components/change-requests/change-requests-table/change-requests-table.tsx
@@ -14,83 +14,18 @@ import './change-requests-table.module.css';
 import ChangeRequestsFilter from '../change-requests-filter/change-requests-filter';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useState } from 'react';
-import {
-  ChangeRequestType,
-  ChangeRequestReason,
-  StandardChangeRequest,
-  ActivationChangeRequest
-} from '../../../utils/src/types/change-request-types';
-
-type FormFieldType = 'select' | 'checkbox';
-
-interface FilterFormField {
-  label: string;
-  type: FormFieldType;
-  values: string[];
-  currentValue: number[];
-}
-
-const filterFieldsList: FilterFormField[] = [
-  {
-    label: 'Requester',
-    type: 'select',
-    values: [],
-    currentValue: [0]
-  },
-  {
-    label: 'Type',
-    type: 'select',
-    values: [''].concat(
-      Object.keys(ChangeRequestType).map(
-        (key) => ChangeRequestType[key as typeof ChangeRequestType.Other]
-      )
-    ),
-    currentValue: [0]
-  },
-  {
-    label: 'Impact',
-    type: 'checkbox',
-    values: ['Scope', 'Budget', 'Impact'],
-    currentValue: []
-  },
-  {
-    label: 'Reason',
-    type: 'checkbox',
-    values: ['']
-      .concat(
-        Object.keys(ChangeRequestReason).map(
-          (key) => ChangeRequestReason[key as typeof ChangeRequestReason.Other]
-        )
-      )
-      .slice(1),
-    currentValue: []
-  },
-  {
-    label: 'State',
-    type: 'select',
-    values: ['', 'Not Reviewed', 'Accepted', 'Denied'],
-    currentValue: [0]
-  },
-  {
-    label: 'Implemented',
-    type: 'select',
-    values: ['', 'Yes', 'No'],
-    currentValue: [0]
-  }
-];
 
 const ChangeRequestsTable: React.FC = () => {
-  const [filterFields, setFilterFields] = useState(filterFieldsList);
+  const [type, setType] = useState('');
+  const [impact, setImpact] = useState([]);
+  const [reason, setReason] = useState('');
+  const [state, setState] = useState([]);
+  const [implemented, setImplemented] = useState('');
   const { isLoading, isError, data, error } = useAllChangeRequests();
 
   if (isLoading) return <LoadingIndicator />;
 
   if (isError) return <ErrorPage message={error?.message} />;
-
-  filterFields[0].values = [
-    ...new Set(data!.map((cr: ChangeRequest) => fullNamePipe(cr.submitter)))
-  ];
-  filterFields[0].values.splice(0, 0, '');
 
   const transformToDisplayChangeRequests = (changeRequests: ChangeRequest[]) => {
     return changeRequests.map((cr: ChangeRequest) => {
@@ -106,72 +41,8 @@ const ChangeRequestsTable: React.FC = () => {
     }) as DisplayChangeRequest[];
   };
 
-  const combineFilters: Function = (filterFields: FilterFormField[]) => {
-    return (cr: ChangeRequest) => {
-      //check if pass each filter, then map result
-      let filterMap = filterFields.map((filterField) => {
-        if (filterField.type === 'checkbox') {
-          if (filterField.currentValue.length === 0) {
-            return true;
-          }
-          let filterVals = filterField.currentValue.map((index) => {
-            return filterField.values[index];
-          });
-          return filterVals
-            .map((value) => {
-              return true; //todo
-            })
-            .every((element) => element);
-        }
-        if (filterField.type === 'select') {
-          if (filterField.values[filterField.currentValue[0]] === '') {
-            return true;
-          } else {
-            let selectValue = filterField.values[filterField.currentValue[0]];
-            if (filterField.label === 'Requester') {
-              return `${cr.submitter.firstName} ${cr.submitter.lastName}` === selectValue;
-            }
-            if (filterField.label === 'Type') {
-              return cr.type === selectValue;
-            }
-            if (filterField.label === 'Implemented') {
-              return (
-                (cr.dateImplemented != undefined && selectValue === 'Yes') ||
-                (cr.dateImplemented === undefined && selectValue === 'No')
-              );
-            }
-          }
-        }
-      });
-      console.log(filterMap);
-      return filterMap.every((element) => element);
-    };
-  };
-
-  const filterChangeRequests = (
-    changeRequests: ChangeRequest[],
-    filterFields: FilterFormField[]
-  ) => {
-    console.log(changeRequests);
-    const filterFunction = combineFilters(filterFields);
-    let val = changeRequests.filter((cr: ChangeRequest) => {
-      console.log(filterFunction(cr));
-      return filterFunction(cr);
-    });
-    console.log(val);
-    return val;
-  };
-
   const changeRequestsTable = () => {
-    const moreData = data!.concat(data!).concat(data!).concat(data!).concat(data!).concat(data!);
-    const lotsData = moreData.concat(moreData);
-    return (
-      <CRTable
-        changeRequests={transformToDisplayChangeRequests(
-          filterChangeRequests(lotsData, filterFields)
-        )}
-      />
-    );
+    return <CRTable changeRequests={transformToDisplayChangeRequests(data!)} />;
   };
 
   const changeRequestsContainer = () => {
@@ -179,10 +50,7 @@ const ChangeRequestsTable: React.FC = () => {
       <Container fluid>
         <Row>
           <Col xs={3}>
-            <ChangeRequestsFilter
-              filterFields={filterFields}
-              setFilterFields={setFilterFields}
-            ></ChangeRequestsFilter>
+            <ChangeRequestsFilter update={sendDataToParent} />
           </Col>
           <Col md={9}>{changeRequestsTable()}</Col>
         </Row>

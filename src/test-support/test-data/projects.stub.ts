75706fef3b36eda9349dd8af3f248adf3c5cf2ca
Ari <80832201+Ari-S-123@users.noreply.github.com>
2021-11-10 16:44:34 -0500

    Revert "#202 - Fixed Merge Conflicts and updated imports from 'utils' to 'utils/src' to deal with errors."
    This reverts commit 212d13d92ec0c59f28919d44372902f21b0da01a.

--- src/test-support/test-data/projects.stub.ts
+++ src/test-support/test-data/projects.stub.ts
@@ -3,7 +3,7 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Project } from 'utils/src';
+import { Project } from 'utils';
 import { WbsElementStatus } from 'utils/src';
 import {
   exampleAdminUser,

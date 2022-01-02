2c31936fb9839416e2f25e94b176314c6ca64bf6
LPfahler Macbook Pro <46872232+lpfahler16@users.noreply.github.com>
2022-01-01 22:08:16 -0500

    #203 - Add filter capabilities for type, state, and implemented

---
+++ src/components/change-requests/change-requests-table/change-requests-filter/change-requests-filter.tsx
@@ -0,0 +1,173 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import React, { ReactElement, useState } from 'react';
+import { Button, Card, Dropdown, Form, FormGroup } from 'react-bootstrap';
+import { ChangeRequestType } from 'utils';
+import { ChangeRequestReason } from 'utils/src';
+import styles from './change-requests-filter.module.css';
+
+interface FilterFieldStateProps {
+  update: (
+    type: string,
+    impact: number[],
+    reason: string,
+    state: number[],
+    implemented: string
+  ) => void;
+}
+
+const ChangeRequestsFilter: React.FC<FilterFieldStateProps> = ({
+  update
+}: FilterFieldStateProps) => {
+  const [type, setType] = useState('');
+  const [impact, setImpact] = useState<number[]>([]);
+  const [reason, setReason] = useState('');
+  const [state, setState] = useState<number[]>([]);
+  const [implemented, setImplemented] = useState('');
+
+  const genDropdownItems = (
+    values: string[],
+    setter: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    const none = (
+      <Dropdown.Item key={'None'} onClick={() => setter('')}>
+        None
+      </Dropdown.Item>
+    );
+    const result: any[] = [none];
+    for (const value of values) {
+      result.push(
+        <Dropdown.Item key={value} onClick={() => setter(value)}>
+          {value}
+        </Dropdown.Item>
+      );
+    }
+    return <>{result}</>;
+  };
+
+  const genCheckboxes = (
+    values: string[],
+    value: number[],
+    setter: React.Dispatch<React.SetStateAction<number[]>>
+  ) => {
+    const toggleValue = (currVal: string): void => {
+      const index = values.indexOf(currVal);
+      const present = value.indexOf(index) !== -1;
+
+      var newVals = [...value];
+      if (present) {
+        newVals = newVals.filter((item) => item !== index);
+      } else {
+        newVals.push(index);
+      }
+
+      setter(newVals);
+    };
+
+    const result: any[] = [];
+    for (const val of values) {
+      result.push(
+        <Form.Check
+          key={val}
+          id={val}
+          type="checkbox"
+          onChange={() => toggleValue(val)}
+          label={val}
+        />
+      );
+    }
+    return <>{result}</>;
+  };
+
+  return (
+    <Card className={styles.card}>
+      <Card.Body>
+        <Card.Title>Filters</Card.Title>
+        <Form>
+          <Form.Group>
+            <Form.Label>Type</Form.Label>
+            <Dropdown className={styles.dropdown}>
+              <Dropdown.Toggle
+                data-testid="type-toggle"
+                variant="light"
+                id="dropdown-split-basic"
+                block={true}
+                className={'text-left ' + styles.dropdownButton}
+              >
+                {type}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="btn-block" align="right">
+                {genDropdownItems(
+                  Object.keys(ChangeRequestType).map(
+                    (key) => ChangeRequestType[key as typeof ChangeRequestType.Other]
+                  ),
+                  setType
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Form.Group>
+          <FormGroup>
+            <Form.Label>Impact</Form.Label>
+            {genCheckboxes(['Scope', 'Budget', 'Impact'], impact, setImpact)}
+          </FormGroup>
+          <Form.Group>
+            <Form.Label>Reason</Form.Label>
+            <Dropdown className={styles.dropdown}>
+              <Dropdown.Toggle
+                data-testid="reason-toggle"
+                variant="light"
+                id="dropdown-split-basic"
+                block={true}
+                className={'text-left ' + styles.dropdownButton}
+              >
+                {reason}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="btn-block" align="right">
+                {genDropdownItems(
+                  Object.keys(ChangeRequestReason).map(
+                    (key) => ChangeRequestReason[key as typeof ChangeRequestReason.Other]
+                  ),
+                  setReason
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Form.Group>
+          <FormGroup>
+            <Form.Label>State</Form.Label>
+            {genCheckboxes(['Not Reviewed', 'Accepted', 'Denied'], state, setState)}
+          </FormGroup>
+          <Form.Group>
+            <Form.Label>Implemented</Form.Label>
+            <Dropdown className={styles.dropdown}>
+              <Dropdown.Toggle
+                data-testid="implemented-toggle"
+                variant="light"
+                id="dropdown-split-basic"
+                block={true}
+                className={'text-left ' + styles.dropdownButton}
+              >
+                {implemented}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="btn-block" align="right">
+                {genDropdownItems(['Yes', 'No'], setImplemented)}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Form.Group>
+          <Button
+            className={styles.applyButton}
+            onClick={() => {
+              update(type, impact, reason, state, implemented);
+            }}
+          >
+            Apply
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ChangeRequestsFilter;
--- src/components/change-requests/change-requests-table/change-requests-table.tsx
+++ src/components/change-requests/change-requests-table/change-requests-table.tsx
@@ -8,24 +8,130 @@ import { booleanPipe, fullNamePipe, wbsPipe } from '../../../shared/pipes';
 import { useAllChangeRequests } from '../../../services/change-requests.hooks';
 import { DisplayChangeRequest } from './change-requests-table/change-requests-table';
 import CRTable from './change-requests-table/change-requests-table'; // Directly rename the default import
+import ChangeRequestsFilter from './change-requests-filter/change-requests-filter';
 import LoadingIndicator from '../../shared/loading-indicator/loading-indicator';
 import ErrorPage from '../../shared/error-page/error-page';
 import './change-requests-table.module.css';
-import ChangeRequestsFilter from '../change-requests-filter/change-requests-filter';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useState } from 'react';
+import { ChangeRequestReason } from 'utils/lib/types/change-request-types';
+import { ChangeRequestType } from 'utils/lib/types/change-request-types';
+
+type FormFieldType = 'select' | 'checkbox';
+
+interface FilterFormField {
+  label: string;
+  type: FormFieldType;
+  values: string[];
+  currentValue: number[];
+}
+
+const filterFieldsList: FilterFormField[] = [
+  {
+    label: 'Type',
+    type: 'select',
+    values: [''].concat(
+      Object.keys(ChangeRequestType).map(
+        (key) => ChangeRequestType[key as typeof ChangeRequestType.Other]
+      )
+    ),
+    currentValue: [0]
+  },
+  {
+    label: 'Impact',
+    type: 'checkbox',
+    values: ['Scope', 'Budget', 'Impact'],
+    currentValue: []
+  },
+  {
+    label: 'Reason',
+    type: 'checkbox',
+    values: ['']
+      .concat(
+        Object.keys(ChangeRequestReason).map(
+          (key) => ChangeRequestReason[key as typeof ChangeRequestReason.Other]
+        )
+      )
+      .slice(1),
+    currentValue: []
+  },
+  {
+    label: 'State',
+    type: 'select',
+    values: ['', 'Not Reviewed', 'Accepted', 'Denied'],
+    currentValue: [0]
+  },
+  {
+    label: 'Implemented',
+    type: 'select',
+    values: ['', 'Yes', 'No'],
+    currentValue: [0]
+  }
+];
 
 const ChangeRequestsTable: React.FC = () => {
   const [type, setType] = useState('');
-  const [impact, setImpact] = useState([]);
+  const [impact, setImpact] = useState<number[]>([]);
   const [reason, setReason] = useState('');
-  const [state, setState] = useState([]);
+  const [state, setState] = useState<number[]>([]);
   const [implemented, setImplemented] = useState('');
   const { isLoading, isError, data, error } = useAllChangeRequests();
 
   if (isLoading) return <LoadingIndicator />;
 
   if (isError) return <ErrorPage message={error?.message} />;
+
+  console.log(type + impact + reason + state + implemented);
+
+  const filterCRs = (
+    changeRequests: ChangeRequest[],
+    type: string,
+    impact: number[],
+    reason: string,
+    state: number[],
+    implemented: string
+  ): ChangeRequest[] => {
+    // Type filter
+    if (type !== '') {
+      changeRequests = changeRequests.filter(
+        (changeRequest: ChangeRequest) => changeRequest.type === type
+      );
+    }
+
+    // // Reason Filter
+    // if (reason !== '') {
+    //   changeRequests = changeRequests.filter(
+    //     (changeRequest: ChangeRequest) => changeRequest.reason === reason
+    //   );
+    // }
+
+    // State filter
+    if (state.length !== 0) {
+      changeRequests = changeRequests.filter((changeRequest: ChangeRequest) => {
+        var filterBool = false;
+        if (state.indexOf(0) !== -1) {
+          filterBool = filterBool || changeRequest.dateReviewed === undefined;
+        }
+        if (state.indexOf(1) !== -1) {
+          filterBool = filterBool || changeRequest.accepted === true;
+        }
+        if (state.indexOf(2) !== -1) {
+          filterBool = filterBool || changeRequest.accepted === false;
+        }
+        return filterBool;
+      });
+    }
+
+    // Implemented Filter
+    if (implemented !== '') {
+      changeRequests = changeRequests.filter(
+        (changeRequest: ChangeRequest) =>
+          (implemented === 'Yes') === (changeRequest.dateImplemented !== undefined)
+      );
+    }
+
+    return changeRequests;
+  };
 
   const transformToDisplayChangeRequests = (changeRequests: ChangeRequest[]) => {
     return changeRequests.map((cr: ChangeRequest) => {
@@ -41,8 +147,28 @@ const ChangeRequestsTable: React.FC = () => {
     }) as DisplayChangeRequest[];
   };
 
+  const sendDataToParent = (
+    type: string,
+    impact: number[],
+    reason: string,
+    state: number[],
+    implemented: string
+  ) => {
+    setType(type);
+    setImpact(impact);
+    setReason(reason);
+    setState(state);
+    setImplemented(implemented);
+  };
+
   const changeRequestsTable = () => {
-    return <CRTable changeRequests={transformToDisplayChangeRequests(data!)} />;
+    return (
+      <CRTable
+        changeRequests={transformToDisplayChangeRequests(
+          filterCRs(data!, type, impact, reason, state, implemented)
+        )}
+      />
+    );
   };
 
   const changeRequestsContainer = () => {

a7b6ddde913b62b306f0293f374331be852ee574
Justin Pong <jp.waves51@gmail.com>
2022-01-15 13:46:47 -0500

    #366 - Add create project endpoint

---
+++ src/backend/functions/projects-new.ts
@@ -0,0 +1,108 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import middy from '@middy/core';
+import jsonBodyParser from '@middy/http-json-body-parser';
+import httpErrorHandler from '@middy/http-error-handler';
+import validator from '@middy/validator';
+import { Handler } from 'aws-lambda';
+import { PrismaClient, WBS_Element_Status } from '@prisma/client';
+import { buildClientFailureResponse, buildNotFoundResponse, buildSuccessResponse } from 'utils';
+
+const prisma = new PrismaClient();
+
+// gets highest current project number
+const getHighestProjectNumber = async (carNumber: number) => {
+  const maxProjectNumber = await prisma.wBS_Element.aggregate({
+    where: {
+      carNumber
+    },
+    max: {
+      projectNumber: true,
+    },
+  });
+
+  return maxProjectNumber.max.projectNumber;
+}
+
+// gets the associated change request for creating a project
+const getChangeRequestReviewState = async (crId: number) => {
+  const cr = await prisma.change_Request.findUnique({
+    where: {
+      crId
+    }
+  });
+
+  // returns null if the change request doesn't exist
+  // if it exists, return a boolean describing if the change request was reviewed
+  return cr ? cr.dateReviewed !== null : cr;
+}
+
+export const baseHandler: Handler = async ({ body }, _context) => {
+  // check if the change request exists
+  const crReviewed = await getChangeRequestReviewState(body.crId);
+  if (crReviewed === null) {
+    return buildNotFoundResponse('change request', `CR #${body.crId}`);
+  }
+
+  // check if the change request for the project was reviewed
+  if (!crReviewed) {
+    return buildClientFailureResponse('Cannot implement an unreview change request');
+  }
+
+  // create the wbs element for the project and document the implemented change request
+  const maxProjectNumber = await getHighestProjectNumber(body.carNumber);
+  const createdProject = await prisma.wBS_Element.create({
+    data: {
+      carNumber: body.carNumber,
+      projectNumber: maxProjectNumber + 1,
+      workPackageNumber: 0,
+      name: body.name,
+      status: WBS_Element_Status.INACTIVE,
+      project: {
+        create: {
+          summary: body.summary,
+        },
+      },
+      changes: {
+        create: {
+          changeRequestId: body.crId,
+          implementerId: body.userId,
+          detail: 'New Project Created',
+        },
+      },
+    },
+    include: {
+      project: true,
+      changes: true,
+    },
+  });
+
+  return buildSuccessResponse(createdProject);
+}
+
+const inputSchema = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        userId: { type: 'integer', minimum: 0 },
+        crId: { type: 'integer', minimum: 0 },
+        name: { type: 'string' },
+        carNumber: { type: 'integer', minimum: 0 },
+        summary: { type: 'string' }
+      },
+      required: ['userId', 'crId', 'name', 'carNumber', 'summary']
+    }
+  }
+};
+
+const handler = middy(baseHandler)
+  .use(jsonBodyParser())
+  .use(validator({ inputSchema }))
+  .use(httpErrorHandler());
+
+export { handler };
---
+++ src/backend/tests/projects-new.test.ts
@@ -0,0 +1,82 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
+import { mockContext } from "../../test-support/test-data/test-utils.stub";
+import { baseHandler, handler } from "../functions/projects-new";
+
+
+describe('projects new', () => {
+  describe('handler', () => {
+    const func = async (event: any) => {
+      return await handler(event, mockContext, () => { });
+    }
+
+    const goodProjectBody = {
+      userId: 1,
+      crId: 1,
+      name: 'title',
+      carNumber: 1,
+      summary: 'some summary'
+    };
+
+    describe('validates project', () => {
+      it('fails with empty body', async () => {
+        const res = await func({ body: {} });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Event object failed validation');
+      });
+
+      it('fails when userId is not a number', async () => {
+        const res = await func({ body: { ...goodProjectBody, userId: 'ayaya' } });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Event object failed validation');
+      });
+
+      it('fails when crId is not a number', async () => {
+        const res = await func({ body: { ...goodProjectBody, crId: 'hohoho' } });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Event object failed validation');
+      });
+
+      it('fails when name is not a string', async () => {
+        const res = await func({ body: { ...goodProjectBody, name: {} } });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Event object failed validation');
+      });
+
+      it('fails when carNumber is not a number', async () => {
+        const res = await func({ body: { ...goodProjectBody, carNumber: 'tee hee' } });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Event object failed validation');
+      });
+
+      it('fails when summary is not a string', async () => {
+        const res = await func({ body: { ...goodProjectBody, summary: {} } });
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Event object failed validation');
+      });
+    });
+
+    describe('base handler', () => {
+      const func = async (body: any) => {
+        return await baseHandler({ body }, mockContext, () => { });
+      };
+
+      it('returns failure when change request not reviewed', async () => {
+        const res = await func({ ...goodProjectBody, crId: 4 });
+        const body = JSON.parse(res.body);
+        expect(res.statusCode).toBe(400);
+        expect(body.message).toBe('Client error: Cannot implement an unreview change request');
+      });
+
+      it('returns failure when change request does not exist', async () => {
+        const res = await func({ ...goodProjectBody, crId: 100000 });
+        const body = JSON.parse(res.body);
+        expect(res.statusCode).toBe(404);
+        expect(body.message).toBe('Could not find the requested change request [CR #100000].');
+      });
+    });
+  });
+});

8514e66206cf18730f584f19e40219372642102b
leoRysing <leshaigor@gmail.com>
2022-01-17 14:33:51 -0500

    Fixed changes part 1

--- src/components/projects/project-edit-form/project-edit-container/project-edit-details/project-edit-details.tsx
+++ src/components/projects/project-edit-form/project-edit-container/project-edit-details/project-edit-details.tsx
@@ -4,78 +4,68 @@
  */
 
 import { Project } from 'utils';
-import {
-  weeksPipe,
-  wbsPipe,
-  endDatePipe,
-  fullNamePipe,
-  listPipe
-} from '../../../../../shared/pipes';
-import PageBlock from '../../../../shared/page-block/page-block';
 import { Col, Container, Row, Form } from 'react-bootstrap';
-// import './work-package-details.module.css';
-import { EditModeContext } from '../project-edit-container';
-import { useContext } from 'react';
+import PageBlock from '../../../../shared/page-block/page-block';
 import ProjectFormDetail from './project-form-detail/project-form-detail';
+// import './work-package-details.module.css';
 
 // new parts added at the bottom
 interface projectDetailsProps {
-    project: Project;
+  project: Project;
 }
 
 const ProjectEditDetails: React.FC<projectDetailsProps> = ({ project }) => {
-    const editMode = useContext(EditModeContext);
-    const detailsBody = (
-      <Container fluid>
-        <Form>
-          <Row>
-            <b>Project Details</b>
-            <Form.Group>
-              <Form.Control>
-                placeholder={"status"}
-                option={"inactive"}
-                option={"active"}
-              </Form.Control>
-            </Form.Group>
-          </Row>
-          <Row>
-            <Col xs={12} md={6}> 
-              <ProjectFormDetail type="title" isReadonly={false} details={project} />
-              <ProjectFormDetail type="wbs" isReadonly={false} details={project} />
-              <ProjectFormDetail type="project-lead"  isReadonly={false} details={project} />
-              <ProjectFormDetail type="project-manager" isReadonly={false} details={project} />
-            </Col>
-            <Col xs={6} md={4}>
-              <ProjectFormDetail type="duration" isReadonly={true} details={project} />
-              <ProjectFormDetail type="start-date" isReadonly={true} details={project} />
-              <ProjectFormDetail type="end-date" isReadonly={true} details={project} />
-              <br />
-              <ProjectFormDetail type="progress" isReadonly={false} details={project} />
-              <b>Timeline Status:</b> <br />
-            </Col>
-          </Row>
-          <br />
-          <br />
-          <Row>
-            <Col>
-              <ProjectFormDetail type="slide-deck" isReadonly={false} details={project}/>
-              <ProjectFormDetail type="task-list" isReadonly={false} details={project}/>
-              <ProjectFormDetail type="bom" isReadonly={false} details={project}/>
-              <ProjectFormDetail type="google-drive" isReadonly={false} details={project}/>
-            </Col>
-          </Row>
-        </Form>
-      </Container>
-    );
-  
-    return (
-      <PageBlock
-        title={'Work Package Details'}
-        headerRight={<b>{project.status}</b>}
-        body={detailsBody}
-      />
-    );
-  };
-  
-  export default ProjectEditDetails;
+  // const editMode = useContext(EditModeContext);
+  const detailsBody = (
+    <Container fluid>
+      <Form>
+        <Row>
+          <b>Project Details</b>
+          <Form.Group>
+            <Form.Control>
+              placeholder={'status'}
+              option={'inactive'}
+              option={'active'}
+            </Form.Control>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <ProjectFormDetail type="title" isReadonly={false} details={project} />
+            <ProjectFormDetail type="wbs" isReadonly={false} details={project} />
+            <ProjectFormDetail type="project-lead" isReadonly={false} details={project} />
+            <ProjectFormDetail type="project-manager" isReadonly={false} details={project} />
+          </Col>
+          <Col xs={6} md={4}>
+            <ProjectFormDetail type="duration" isReadonly={true} details={project} />
+            <ProjectFormDetail type="start-date" isReadonly={true} details={project} />
+            <ProjectFormDetail type="end-date" isReadonly={true} details={project} />
+            <br />
+            <ProjectFormDetail type="progress" isReadonly={false} details={project} />
+            <b>Timeline Status:</b> <br />
+          </Col>
+        </Row>
+        <br />
+        <br />
+        <Row>
+          <Col>
+            <ProjectFormDetail type="slide-deck" isReadonly={false} details={project} />
+            <ProjectFormDetail type="task-list" isReadonly={false} details={project} />
+            <ProjectFormDetail type="bom" isReadonly={false} details={project} />
+            <ProjectFormDetail type="google-drive" isReadonly={false} details={project} />
+          </Col>
+        </Row>
+      </Form>
+    </Container>
+  );
 
+  return (
+    <PageBlock
+      title={'Project Edit Form Details'}
+      headerRight={<b>{project.status}</b>}
+      body={detailsBody}
+    />
+  );
+};
+
+export default ProjectEditDetails;
--- src/components/projects/project-edit-form/project-edit-container/project-edit-details/project-form-detail/project-form-detail.tsx
+++ src/components/projects/project-edit-form/project-edit-container/project-edit-details/project-form-detail/project-form-detail.tsx
@@ -11,91 +11,107 @@ import {
 import { Form } from 'react-bootstrap';
 
 interface ProjectFormDetailProps {
-    type: string;
-    isReadonly: boolean;
-    details: Project;
+  type: string;
+  isReadonly: boolean;
+  details: Project;
 }
-  
+
 class Modifier {
-    title: string;
-    inputType: string;
-    value: any;
-    constructor(title: string, inputType: string, value: any) {
-      this.title = title;
-      this.inputType = inputType;
-      this.value = value;
-    }
+  title: string;
+  inputType: string;
+  value: any;
+  constructor(title: string, inputType: string, value: any) {
+    this.title = title;
+    this.inputType = inputType;
+    this.value = value;
+  }
 }
-  
-  const ProjectFormDetail: React.FC<ProjectFormDetailProps> = ({ type, isReadonly, details }) => {
-    const editMode = useContext(EditModeContext);
-    const workPkg = details.workPackages[0];
-    const typeModifier = new Map([
-      ['title', new Modifier('Work Package Name:', 'text', details.name)],
-      ['wbs', new Modifier('WBS #:', 'text', wbsPipe(details.wbsNum))],
-      [
-        'project-lead',
-        new Modifier('Project Lead:', 'text', listPipe(details.projectLead, fullNamePipe))
-      ],
-      [
-        'project-manager',
-        new Modifier('Project Manager: ', 'text', fullNamePipe(details.projectManager))
-      ],
-      ['duration', new Modifier('Duration: ', 'number', weeksPipe(workPkg.duration))],
-      [
-        'start-date',
-        new Modifier('Start Date: ', 'date', workPkg.startDate.toLocaleDateString())
-      ],
-      [
-        'end-date',
-        new Modifier('End Date: ', 'date', endDatePipe(workPkg.startDate, workPkg.duration))
-      ],
-      ['progress', new Modifier('Progress: ', 'number', `${workPkg.progress}%`)],
-      ['slide-deck', new Modifier('Slide Deck: ', 'link', 
-          (details.slideDeckLink == "") ? 'Enter Link Here...' : details.slideDeckLink)],
-      ['task-list', new Modifier('Task List: ', 'link', 
-          (details.taskListLink == "") ? 'Enter Link Here...' : details.taskListLink)],
-      ['bom', new Modifier('BOM: ', 'link', 
-          (details.bomLink == "") ? 'Enter Link Here...' : details.bomLink)],
-      ['google-drive', new Modifier('Google Drive: ', 'link', 
-          (details.gDriveLink == "") ? 'Enter Link Here...' : details.gDriveLink)]
-    ]);
 
-    /* not the ideal implementation of this; I just don't know a better way to implement
+const ProjectFormDetail: React.FC<ProjectFormDetailProps> = ({ type, isReadonly, details }) => {
+  const editMode = useContext(EditModeContext);
+  const workPkg = details.workPackages[0];
+  const typeModifier = new Map([
+    ['title', new Modifier('Work Package Name:', 'text', details.name)],
+    ['wbs', new Modifier('WBS #:', 'text', wbsPipe(details.wbsNum))],
+    [
+      'project-lead',
+      new Modifier('Project Lead:', 'text', listPipe(details.projectLead, fullNamePipe))
+    ],
+    [
+      'project-manager',
+      new Modifier('Project Manager: ', 'text', fullNamePipe(details.projectManager))
+    ],
+    ['duration', new Modifier('Duration: ', 'number', weeksPipe(workPkg.duration))],
+    ['start-date', new Modifier('Start Date: ', 'date', workPkg.startDate.toLocaleDateString())],
+    [
+      'end-date',
+      new Modifier('End Date: ', 'date', endDatePipe(workPkg.startDate, workPkg.duration))
+    ],
+    ['progress', new Modifier('Progress: ', 'number', `${workPkg.progress}%`)],
+    [
+      'slide-deck',
+      new Modifier(
+        'Slide Deck: ',
+        'link',
+        details.slideDeckLink === '' ? 'Enter Link Here...' : details.slideDeckLink
+      )
+    ],
+    [
+      'task-list',
+      new Modifier(
+        'Task List: ',
+        'link',
+        details.taskListLink === '' ? 'Enter Link Here...' : details.taskListLink
+      )
+    ],
+    [
+      'bom',
+      new Modifier('BOM: ', 'link', details.bomLink === '' ? 'Enter Link Here...' : details.bomLink)
+    ],
+    [
+      'google-drive',
+      new Modifier(
+        'Google Drive: ',
+        'link',
+        details.gDriveLink === '' ? 'Enter Link Here...' : details.gDriveLink
+      )
+    ]
+  ]);
+
+  /* not the ideal implementation of this; I just don't know a better way to implement
        the functionality of conditionally making an entry readonly. */
-    if (isReadonly) {
-      return (
-        <Form.Group>
-          <b>{typeModifier.get(type)?.title}</b>{' '}
-          {editMode ? (
-            <Form.Control
-              type={typeModifier.get(type)?.inputType}
-              placeholder={typeModifier.get(type)?.value}
-              readOnly
-            />
-          ) : (
-            typeModifier.get(type)?.value
-          )}
-          <br />
-        </Form.Group>
-      ); 
-    } else {
-      return (
-        <Form.Group>
-          <b>{typeModifier.get(type)?.title}</b>{' '}
-          {editMode ? (
-            <Form.Control
-              type={typeModifier.get(type)?.inputType}
-              placeholder={typeModifier.get(type)?.value}
-            />
-          ) : (
-            typeModifier.get(type)?.value
-          )}
-          <br />
-        </Form.Group>
-      ); 
-    }
+  if (isReadonly) {
+    return (
+      <Form.Group>
+        <b>{typeModifier.get(type)?.title}</b>{' '}
+        {editMode ? (
+          <Form.Control
+            type={typeModifier.get(type)?.inputType}
+            placeholder={typeModifier.get(type)?.value}
+            readOnly
+          />
+        ) : (
+          typeModifier.get(type)?.value
+        )}
+        <br />
+      </Form.Group>
+    );
+  } else {
+    return (
+      <Form.Group>
+        <b>{typeModifier.get(type)?.title}</b>{' '}
+        {editMode ? (
+          <Form.Control
+            type={typeModifier.get(type)?.inputType}
+            placeholder={typeModifier.get(type)?.value}
+          />
+        ) : (
+          typeModifier.get(type)?.value
+        )}
+        <br />
+      </Form.Group>
+    );
+  }
+};
 
-  };
-  
-  export default ProjectFormDetail;
+export default ProjectFormDetail;

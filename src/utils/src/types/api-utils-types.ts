c3a391e9b08ff8c2b41f4f6ad416470f226c97f5
AC-Dc <changdavidson4@gmail.com>
2022-02-02 13:58:08 -0500

    #488 - add schema creating helpers

--- src/utils/src/types/api-utils-types.ts
+++ src/utils/src/types/api-utils-types.ts
@@ -16,3 +16,43 @@ export interface ApiRoute {
   httpMethod: string;
   func: ApiRouteFunction;
 }
+
+export const eventSchema = <Body extends Record<string, any>>(
+  bodySchema: Body,
+  methods?: string[]
+) =>
+  ({
+    type: 'object',
+    properties: {
+      httpMethod: {
+        type: 'string',
+        enum: methods
+      },
+      body: bodySchema,
+      headers: {
+        type: 'object',
+        properties: {
+          'user-agent': { type: 'string' }
+          // Cookie: { type: 'string' }
+        },
+        required: ['user-agent']
+      }
+    },
+    required: ['body', 'headers', 'httpMethod']
+  } as const);
+
+export const bodySchema = <Props extends Record<string, any>>(
+  properties: Props,
+  optionalProps: (keyof Props)[] = [],
+  additionalProperties = false
+) =>
+  ({
+    type: 'object',
+    properties,
+    required: Object.keys(properties).filter((key: keyof Props) => !optionalProps.includes(key)),
+    additionalProperties
+  } as const);
+
+export const intType = { type: 'integer', minimum: 0 } as const;
+export const stringType = { type: 'string' } as const;
+export const dateType = { type: 'string', format: 'date' } as const;

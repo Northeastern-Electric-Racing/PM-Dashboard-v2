72a0796688a13c1d5093828db12bed2a7a920070
leoRysing <leshaigor@gmail.com>
2022-02-07 20:52:10 -0500

    #439 - change to formContext component

--- src/components/projects/project-edit-form/project-edit-container/project-edit-container.tsx
+++ src/components/projects/project-edit-form/project-edit-container/project-edit-container.tsx
@@ -3,9 +3,10 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { createContext, useState, useEffect } from 'react';
+import { createContext, SyntheticEvent, useState, useEffect } from 'react';
 import { WbsNumber } from 'utils';
 import { wbsPipe } from '../../../../shared/pipes';
+import { Form } from 'react-bootstrap';
 import { useSingleProject } from '../../../../services/projects.hooks';
 import LoadingIndicator from '../../../shared/loading-indicator/loading-indicator';
 import DescriptionList from '../../../shared/description-list/description-list';
@@ -14,7 +15,10 @@ import PageTitle from '../../../shared/page-title/page-title';
 import ProjectEditDetails from './project-edit-details/project-edit-details';
 import EditModeOptions from './edit-mode-options/edit-mode-options';
 
-export const EditModeContext = createContext(false);
+export const FormContext = createContext({
+  editMode: false,
+  setField: (field: string, value: any) => {}
+});
 
 interface EditFormContainerProps {
   wbsNum: WbsNumber;
@@ -24,29 +28,48 @@ export interface EditModeProps {
   changeEditMode(arg: any): void;
 }
 
-const WorkPackageContainer: React.FC<EditFormContainerProps> = ({ wbsNum }) => {
+const ProjectEditContainer: React.FC<EditFormContainerProps> = ({ wbsNum }) => {
   const { isLoading, isError, data, error } = useSingleProject(wbsNum);
   const [editMode, setEditMode] = useState(false);
+  const [form, setForm] = useState({});
+  const [validated, setValidated] = useState(false);
+
+  const setField = (field: string, value: any) => {
+    setForm({
+      ...form,
+      [field]: value
+    });
+  };
 
   useEffect(() => {
-    setEditMode(false);
-  }, [wbsNum]);
+    console.log(form);
+  }, [form]);
+
+  const value = { editMode, setField };
 
   if (isLoading) return <LoadingIndicator />;
 
   if (isError) return <ErrorPage message={error?.message} />;
 
+  const handleSubmit = (event: SyntheticEvent) => {
+    //event.preventDefault();
+    const { currentTarget } = event;
+    console.log(currentTarget);
+  };
+
   return (
-    <EditModeContext.Provider value={editMode}>
+    <FormContext.Provider value={value}>
       <div className="mb-5">
-        <PageTitle title={`${wbsPipe(wbsNum)} - ${data!.name}`} />
-        <ProjectEditDetails project={data!} />
-        <DescriptionList title={'Goals'} items={data!.goals} />
-        <DescriptionList title={'Features'} items={data!.features} />
-        {editMode ? <EditModeOptions changeEditMode={() => setEditMode(false)} /> : ''}
+        <Form onSubmit={handleSubmit}>
+          <PageTitle title={`${wbsPipe(wbsNum)} - ${data!.name}`} />
+          <ProjectEditDetails project={data!} />
+          <DescriptionList title={'Goals'} items={data!.goals} />
+          <DescriptionList title={'Features'} items={data!.features} />
+          {editMode ? <EditModeOptions changeEditMode={() => setEditMode(false)} /> : ''}
+        </Form>
       </div>
-    </EditModeContext.Provider>
+    </FormContext.Provider>
   );
 };
 
-export default WorkPackageContainer;
+export default ProjectEditContainer;

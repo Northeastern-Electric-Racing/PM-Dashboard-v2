a06d33989b14ca432545be962c85326f1fd35fe6
Justin Pong <jp.waves51@gmail.com>
2022-03-16 18:43:56 -0400

    Revert "#565 fix tests"
    This reverts commit cab7f0cfc2bf47707d5440b527f6acddcf034a2a.

--- src/components/change-requests/review-change-request/review-change-request/review-change-request.test.tsx
+++ src/components/change-requests/review-change-request/review-change-request/review-change-request.test.tsx
@@ -3,59 +3,51 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { exampleStandardChangeRequest } from '../../../../test-support/test-data/change-requests.stub';
-import { render, screen, routerWrapperBuilder } from '../../../../test-support/test-utils';
-import ReviewChangeRequestsView from './review-change-request';
-
-const mockHandleSubmit = jest.fn();
-const mockHandleCancel = jest.fn();
+import { exampleStandardChangeRequest } from "../../../../test-support/test-data/change-requests.stub";
+import { render, screen, routerWrapperBuilder } from "../../../../test-support/test-utils";
+import ReviewChangeRequestsView from "./review-change-request";
 
 /**
  * Sets up the component under test with the desired values and renders it.
  */
 const renderComponent = (option: 'Accept' | 'Deny') => {
-  const RouterWrapper = routerWrapperBuilder({});
-  return render(
-    <RouterWrapper>
-      <ReviewChangeRequestsView
-        crId={exampleStandardChangeRequest.crId}
-        option={option}
-        onSubmit={mockHandleSubmit}
-        onCancel={mockHandleCancel}
-      />
-    </RouterWrapper>
-  );
+    const RouterWrapper = routerWrapperBuilder({});
+    return render(
+        <RouterWrapper>
+            <ReviewChangeRequestsView crId={exampleStandardChangeRequest.crId} option={option} />
+        </RouterWrapper>
+    );
 };
 
-describe('review change request page test suite', () => {
-  it('renders accept title', () => {
-    renderComponent('Accept');
+describe('review change request page', () => {
+    it('renders accept title', () => {
+        renderComponent('Accept');
 
-    expect(screen.queryByText('Accept Change Request')).toBeInTheDocument();
-  });
+        expect(screen.queryByText('Accept Change Request')).toBeInTheDocument();
+    });
 
-  it('renders deny title', () => {
-    renderComponent('Deny');
+    it('renders deny title', () => {
+        renderComponent('Deny');
 
-    expect(screen.queryByText('Deny Change Request')).toBeInTheDocument();
-  });
+        expect(screen.queryByText('Deny Change Request')).toBeInTheDocument();
+    });
 
-  it('renders label for textbox', () => {
-    renderComponent('Accept');
+    it('renders label for textbox', () => {
+        renderComponent('Accept');
 
-    expect(screen.getByLabelText('Review Notes')).toBeInTheDocument();
-  });
+        expect(screen.getByLabelText('Review Notes')).toBeInTheDocument();
+    });
 
-  it('renders textbox', () => {
-    renderComponent('Accept');
+    it('renders textbox', () => {
+        renderComponent('Accept');
 
-    expect(screen.getByPlaceholderText('Notes...')).toBeInTheDocument();
-  });
+        expect(screen.getByPlaceholderText('Notes...')).toBeInTheDocument();
+    });
 
-  it('renders buttons', () => {
-    renderComponent('Accept');
+    it('renders buttons', () => {
+        renderComponent('Accept');
 
-    expect(screen.getByText('Cancel')).toBeInTheDocument();
-    expect(screen.getByText('Confirm')).toBeInTheDocument();
-  });
+        expect(screen.getByText('Cancel')).toBeInTheDocument();
+        expect(screen.getByText('Confirm')).toBeInTheDocument();
+    });
 });

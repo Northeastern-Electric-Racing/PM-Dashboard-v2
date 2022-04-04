a73ecd9d3f3f9a86b32856adda2e71566ab00773
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-04-04 14:27:57 -0400

    #369 - Clean up imports and formatting

--- src/components/projects/wbs-details/work-package-container/work-package-container-view/work-package-details/work-package-details.tsx
+++ src/components/projects/wbs-details/work-package-container/work-package-container-view/work-package-details/work-package-details.tsx
@@ -3,15 +3,14 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { WorkPackage } from 'utils';
+import { useContext } from 'react';
+import { Col, Container, Row, Form } from 'react-bootstrap';
+import { WorkPackage, WbsElementStatus } from 'utils';
+import { FormContext } from '../../work-package-container';
 import { wbsPipe, endDatePipe, fullNamePipe, percentPipe } from '../../../../../../shared/pipes';
 import PageBlock from '../../../../../shared/page-block/page-block';
-import { Col, Container, Row, Form } from 'react-bootstrap';
-import './work-package-details.module.css';
 import EditableDetail from '../../../../../shared/editable-detail/editable-detail';
-import { FormContext } from '../../work-package-container';
-import { useContext } from 'react';
-import { WbsElementStatus } from 'utils/lib/types/project-types';
+import './work-package-details.module.css';
 
 interface WorkPackageDetailsProps {
   workPackage: WorkPackage;
@@ -28,67 +27,67 @@ const WorkPackageDetails: React.FC<WorkPackageDetailsProps> = ({ workPackage }) 
 
   const detailsBody = (
     <Container fluid>
-        <Row>
-          <Col xs={12} md={6}>
-            <EditableDetail title="Work Package Name" value={workPackage.name} type="text" />
-            <EditableDetail
-              title="WBS #"
-              value={wbsPipe(workPackage.wbsNum)}
-              type="text"
-              readOnly={true}
-            />
-            <EditableDetail
-              title="Project Lead"
-              value={fullNamePipe(workPackage.projectLead)}
-              type="text"
-            />
-            <EditableDetail
-              title="Project Manager"
-              value={fullNamePipe(workPackage.projectManager)}
-              type="text"
-            />
-            <EditableDetail
-              title="Duration"
-              value={`${workPackage.duration}`}
-              type="number"
-              suffix="weeks"
-            />
-          </Col>
-          <Col xs={6} md={4}>
-            <EditableDetail
-              title="Start Date"
-              value={
-                editMode
-                  ? formatDate(workPackage.startDate) // for a date input, format must be yyyy-mm-dd
-                  : workPackage.startDate.toLocaleDateString()
-              }
-              type="date"
-            />
-            <EditableDetail
-              title="End Date"
-              value={endDatePipe(workPackage.startDate, workPackage.duration)}
-              type="date"
-              readOnly={true}
-            />
-            <EditableDetail
-              title="Progress"
-              value={percentPipe(workPackage.progress)}
-              type="number"
-            />
-            <EditableDetail
-              title="Expected Progress"
-              value={percentPipe(workPackage.expectedProgress)}
-              type="number"
-              readOnly={true}
-            />
-            <EditableDetail
-              title="Timeline Status"
-              value={workPackage.timelineStatus}
-              type="text"
-              readOnly={true}
-            />
-          </Col>
-        </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <EditableDetail title="Work Package Name" value={workPackage.name} type="text" />
+          <EditableDetail
+            title="WBS #"
+            value={wbsPipe(workPackage.wbsNum)}
+            type="text"
+            readOnly={true}
+          />
+          <EditableDetail
+            title="Project Lead"
+            value={fullNamePipe(workPackage.projectLead)}
+            type="text"
+          />
+          <EditableDetail
+            title="Project Manager"
+            value={fullNamePipe(workPackage.projectManager)}
+            type="text"
+          />
+          <EditableDetail
+            title="Duration"
+            value={`${workPackage.duration}`}
+            type="number"
+            suffix="weeks"
+          />
+        </Col>
+        <Col xs={6} md={4}>
+          <EditableDetail
+            title="Start Date"
+            value={
+              editMode
+                ? formatDate(workPackage.startDate) // for a date input, format must be yyyy-mm-dd
+                : workPackage.startDate.toLocaleDateString()
+            }
+            type="date"
+          />
+          <EditableDetail
+            title="End Date"
+            value={endDatePipe(workPackage.startDate, workPackage.duration)}
+            type="date"
+            readOnly={true}
+          />
+          <EditableDetail
+            title="Progress"
+            value={percentPipe(workPackage.progress)}
+            type="number"
+          />
+          <EditableDetail
+            title="Expected Progress"
+            value={percentPipe(workPackage.expectedProgress)}
+            type="number"
+            readOnly={true}
+          />
+          <EditableDetail
+            title="Timeline Status"
+            value={workPackage.timelineStatus}
+            type="text"
+            readOnly={true}
+          />
+        </Col>
+      </Row>
     </Container>
   );
 

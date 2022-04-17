6fc3aaecdbec0ef1c7ce5dcb824fdda895a70d11
RChandler234 <reid.c.chandler@gmail.com>
2022-04-17 18:31:01 -0400

    #473 updating PR to work with current codebase

--- src/components/projects/wbs-details/work-package-container/work-package-container-view/changes-list/changes-list.tsx
+++ src/components/projects/wbs-details/work-package-container/work-package-container-view/changes-list/changes-list.tsx
@@ -5,11 +5,11 @@
 
 import { Link } from 'react-router-dom';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { datePipe, fullNamePipe, wbsPipe } from '../../../../../shared/pipes';
 import { ImplementedChange } from 'utils';
 import { routes } from '../../../../../../shared/routes';
 import BulletList from '../../../../../shared/bullet-list/bullet-list';
 import './changes-list.module.css';
+import { datePipe, fullNamePipe } from '../../../../../../shared/pipes';
 
 interface ChangesListProps {
   changes: ImplementedChange[];

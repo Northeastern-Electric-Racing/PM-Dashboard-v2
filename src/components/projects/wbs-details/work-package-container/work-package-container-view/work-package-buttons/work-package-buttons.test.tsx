516097ec291926f0e35e86281c8422549d8699a0
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-05-14 23:08:57 -0400

    #613 - abstract out render to separate function for tests

--- src/components/projects/wbs-details/work-package-container/work-package-container-view/work-package-buttons/work-package-buttons.test.tsx
+++ src/components/projects/wbs-details/work-package-container/work-package-container-view/work-package-buttons/work-package-buttons.test.tsx
@@ -3,28 +3,29 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../../../../test-support/test-utils';
 import { FormContext } from '../work-package-container-edit';
 import WorkPackageButtons from './work-package-buttons';
 
+const renderComponent = (editMode: boolean) => {
+  return render(
+    <FormContext.Provider value={{ editMode, setField: (field: string, value: any) => {} }}>
+      <WorkPackageButtons setEditMode={() => {}} />
+    </FormContext.Provider>
+  );
+};
+
 describe.skip('Work package edit buttons', () => {
-  const setField = (field: string, value: any) => {};
   it('renders all of the buttons, with edit mode enabled', () => {
-    render(
-      <FormContext.Provider value={{ editMode: false, setField }}>
-        <WorkPackageButtons setEditMode={() => {}} />
-      </FormContext.Provider>
-    );
+    renderComponent(false);
+
     expect(screen.getByText('New Change Request')).toBeInTheDocument();
     expect(screen.getByText('Edit')).toBeInTheDocument();
     expect(screen.getByText('Edit')).toBeEnabled();
   });
   it('renders all of the buttons, with edit mode disabled', () => {
-    render(
-      <FormContext.Provider value={{ editMode: true, setField }}>
-        <WorkPackageButtons setEditMode={() => {}} />
-      </FormContext.Provider>
-    );
+    renderComponent(true);
+
     expect(screen.getByText('New Change Request')).toBeInTheDocument();
     expect(screen.getByText('Edit')).toBeInTheDocument();
     expect(screen.getByText('Edit')).toBeDisabled();

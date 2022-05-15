00bf694c5602ec9634f902033bc745fbcd7d7bcd
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-05-14 23:26:21 -0400

    #613 - fix and unskip tests

--- src/components/projects/wbs-details/work-package-container/work-package-container-view/work-package-container-view.test.tsx
+++ src/components/projects/wbs-details/work-package-container/work-package-container-view/work-package-container-view.test.tsx
@@ -8,38 +8,28 @@ import { exampleWorkPackage2 } from '../../../../../test-support/test-data/work-
 import WorkPackageContainerView from './work-package-container-view';
 
 // Sets up the component under test with the desired values and renders it.
-const renderComponent = (edit: boolean) => {
+const renderComponent = () => {
   const RouterWrapper = routerWrapperBuilder({});
   return render(
     <RouterWrapper>
       <WorkPackageContainerView
         workPackage={exampleWorkPackage2}
         edit={{ editMode: false, setEditMode: () => null }}
+        allowEdit={true}
       />
     </RouterWrapper>
   );
 };
 
-describe.skip('work package container view', () => {
-  it('renders the project in read-only mode', () => {
-    renderComponent(false);
+describe('work package container view', () => {
+  it('renders the project', () => {
+    renderComponent();
 
     expect(screen.getByText('1.1.2 - Adhesive Shear Strength Test')).toBeInTheDocument();
     expect(screen.getByText('Dependencies')).toBeInTheDocument();
     expect(screen.getByText('Expected Activities')).toBeInTheDocument();
-    expect(screen.getByText('Delieverables')).toBeInTheDocument();
+    expect(screen.getByText('Deliverables')).toBeInTheDocument();
     expect(screen.getByText('Edit')).toBeEnabled();
     expect(screen.queryByText('Save')).not.toBeInTheDocument();
-  });
-
-  it('renders the project in edit mode', () => {
-    renderComponent(true);
-
-    expect(screen.getByText('1.1.2 - Adhesive Shear Strength Test')).toBeInTheDocument();
-    expect(screen.getByText('Dependencies')).toBeInTheDocument();
-    expect(screen.getByText('Expected Activities')).toBeInTheDocument();
-    expect(screen.getByText('Delieverables')).toBeInTheDocument();
-    expect(screen.getByText('Edit')).toBeDisabled();
-    expect(screen.getByText('Save')).toBeInTheDocument();
   });
 });

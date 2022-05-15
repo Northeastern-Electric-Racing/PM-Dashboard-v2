8e026cc5ceadc23947551b63a928a22a0eef809f
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-05-14 22:47:55 -0400

    #613 - Disable new wp form for guest users

--- src/components/projects/create-wp-form/create-wp-form.test.tsx
+++ src/components/projects/create-wp-form/create-wp-form.test.tsx
@@ -4,7 +4,19 @@
  */
 
 import { render, screen } from '../../../test-support/test-utils';
+import { useAuth } from '../../../services/auth.hooks';
+import { Auth } from '../../../shared/types';
+import { exampleAdminUser, exampleGuestUser } from '../../../test-support/test-data/users.stub';
+import { mockAuth } from '../../../test-support/test-data/test-utils.stub';
 import CreateWPForm from './create-wp-form';
+
+jest.mock('../../../services/auth.hooks');
+
+const mockedUseAuth = useAuth as jest.Mock<Auth>;
+
+const mockAuthHook = (user = exampleAdminUser) => {
+  mockedUseAuth.mockReturnValue(mockAuth(user));
+};
 
 /**
  * Sets up the component under test with the desired values and renders it.
@@ -15,8 +27,23 @@ const renderComponent = () => {
 
 describe('create wp form test suite', () => {
   it('render view component', () => {
+    mockAuthHook();
     renderComponent();
 
     expect(screen.getByText('Create New Work Package')).toBeInTheDocument();
+  });
+
+  it('disables submit button for guest users', () => {
+    mockAuthHook(exampleGuestUser);
+    renderComponent();
+
+    expect(screen.getByText('Create')).toBeDisabled();
+  });
+
+  it('enables submit button for admin users', () => {
+    mockAuthHook();
+    renderComponent();
+
+    expect(screen.getByText('Create')).not.toBeDisabled();
   });
 });

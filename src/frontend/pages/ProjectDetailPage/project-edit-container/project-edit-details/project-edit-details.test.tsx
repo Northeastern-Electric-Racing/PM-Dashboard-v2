7af0aa15cc0b791d062c58603912c8571cb24cf0
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-05-27 16:24:04 -0400

    #677 - add file license text

--- src/frontend/pages/ProjectDetailPage/project-edit-container/project-edit-details/project-edit-details.test.tsx
+++ src/frontend/pages/ProjectDetailPage/project-edit-container/project-edit-details/project-edit-details.test.tsx
@@ -1,3 +1,8 @@
+/*
+ * This file is part of NER's PM Dashboard and licensed under GNU AGPLv3.
+ * See the LICENSE file in the repository root folder for details.
+ */
+
 import { act, render, screen } from '../../../../../test-support/test-utils';
 import userEvent from '@testing-library/user-event';
 import { endDatePipe, fullNamePipe, wbsPipe } from '../../../../../shared/pipes';

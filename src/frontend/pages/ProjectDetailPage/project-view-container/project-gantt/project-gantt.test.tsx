4d2cea1357a5ed4132998a32c135f199d777639d
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-06-16 03:00:16 -0400

    #596 - minimize tests to ensure flexibility

--- src/frontend/pages/ProjectDetailPage/project-view-container/project-gantt/project-gantt.test.tsx
+++ src/frontend/pages/ProjectDetailPage/project-view-container/project-gantt/project-gantt.test.tsx
@@ -3,37 +3,18 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { render, screen, routerWrapperBuilder } from '../../../../../test-support/test-utils';
-import { useTheme } from '../../../../../services/theme.hooks';
-import themes from '../../../../../shared/themes';
-import { Theme } from '../../../../../shared/types';
+import { render, screen } from '../../../../../test-support/test-utils';
 import { exampleAllWorkPackages } from '../../../../../test-support/test-data/work-packages.stub';
 import ProjectGantt from './project-gantt';
 
-jest.mock('../../../../../services/theme.hooks');
-const mockTheme = useTheme as jest.Mock<Theme>;
-
-const mockHook = () => {
-  mockTheme.mockReturnValue(themes[0]);
-};
-
 // Sets up the component under test with the desired values and renders it.
 const renderComponent = () => {
-  const RouterWrapper = routerWrapperBuilder({});
-  return render(
-    <RouterWrapper>
-      <ProjectGantt workPackages={exampleAllWorkPackages} />
-    </RouterWrapper>
-  );
+  return render(<ProjectGantt workPackages={exampleAllWorkPackages} />);
 };
 
 describe('project gantt component', () => {
-  beforeEach(() => mockHook());
-
   it('Renders title', () => {
     renderComponent();
     expect(screen.getByText('Gantt Chart')).toBeInTheDocument();
   });
-
-  it.todo('test display work packages');
 });

9e9f9d7fefe15b9de96e4bf342dba90d702d762f
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-06-18 02:57:03 -0400

    #729 - switch from cols to content-fitting cards

--- src/frontend/pages/HomePage/upcoming-deadlines/upcoming-deadlines.tsx
+++ src/frontend/pages/HomePage/upcoming-deadlines/upcoming-deadlines.tsx
@@ -3,7 +3,7 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../../../../services/theme.hooks';
 import { useAllWorkPackagesUpcomingDeadlines } from '../../../../services/work-packages.hooks';
@@ -12,7 +12,7 @@ import { routes } from '../../../../shared/routes';
 import LoadingIndicator from '../../../components/loading-indicator/loading-indicator';
 import PageBlock from '../../../layouts/page-block/page-block';
 import ErrorPage from '../../ErrorPage/error-page';
-import './upcoming-deadlines.module.css';
+import styles from './upcoming-deadlines.module.css';
 
 const UpcomingDeadlines: React.FC = () => {
   const theme = useTheme();
@@ -23,32 +23,34 @@ const UpcomingDeadlines: React.FC = () => {
   }
 
   const fullDisplay = (
-    <Row className="flex-nowrap overflow-auto">
+    <Row className="flex-nowrap overflow-auto justify-content-start">
       {workPackages.data?.length === 0
         ? 'No upcoming deadlines'
         : workPackages.data?.map((wp) => (
-            <Col className="px-1" xs={12} sm={7} md={5} lg={4} xl={3}>
-              <Card border={theme.cardBorder} bg={theme.cardBg}>
-                <Card.Body className="p-3">
-                  <Card.Title className="mb-2">
-                    <Link to={`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`}>
-                      {wbsPipe(wp.wbsNum)} - {wp.name}
-                    </Link>
-                  </Card.Title>
-                  <Card.Text>
-                    <Container fluid>
-                      <Row className="pb-1">End Date: {datePipe(wp.endDate)}</Row>
-                      <Row className="pb-1">Engineering Lead: {fullNamePipe(wp.projectLead)}</Row>
-                      <Row className="pb-1">Project Manager: {fullNamePipe(wp.projectManager)}</Row>
-                      <Row>
-                        {wp.expectedActivities.length} Expected Activities, {wp.deliverables.length}{' '}
-                        Deliverables
-                      </Row>
-                    </Container>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
+            <Card
+              className={styles.upcomingDeadlineCard}
+              border={theme.cardBorder}
+              bg={theme.cardBg}
+            >
+              <Card.Body className="p-3">
+                <Card.Title className="mb-2">
+                  <Link to={`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`}>
+                    {wbsPipe(wp.wbsNum)} - {wp.name}
+                  </Link>
+                </Card.Title>
+                <Card.Text>
+                  <Container fluid>
+                    <Row className="pb-1">End Date: {datePipe(wp.endDate)}</Row>
+                    <Row className="pb-1">Engineering Lead: {fullNamePipe(wp.projectLead)}</Row>
+                    <Row className="pb-1">Project Manager: {fullNamePipe(wp.projectManager)}</Row>
+                    <Row>
+                      {wp.expectedActivities.length} Expected Activities, {wp.deliverables.length}{' '}
+                      Deliverables
+                    </Row>
+                  </Container>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           ))}
     </Row>
   );

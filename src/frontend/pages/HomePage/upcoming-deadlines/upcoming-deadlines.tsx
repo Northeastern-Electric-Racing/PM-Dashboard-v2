cea2aee2bf9d6c88808748656fdcedbe083a5d20
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-06-18 02:42:56 -0400

    #729 - comprehensive testing for upcoming deadlines

--- src/frontend/pages/HomePage/upcoming-deadlines/upcoming-deadlines.tsx
+++ src/frontend/pages/HomePage/upcoming-deadlines/upcoming-deadlines.tsx
@@ -9,47 +9,54 @@ import { useTheme } from '../../../../services/theme.hooks';
 import { useAllWorkPackagesUpcomingDeadlines } from '../../../../services/work-packages.hooks';
 import { datePipe, wbsPipe, fullNamePipe } from '../../../../shared/pipes';
 import { routes } from '../../../../shared/routes';
+import LoadingIndicator from '../../../components/loading-indicator/loading-indicator';
 import PageBlock from '../../../layouts/page-block/page-block';
+import ErrorPage from '../../ErrorPage/error-page';
 import './upcoming-deadlines.module.css';
 
 const UpcomingDeadlines: React.FC = () => {
   const theme = useTheme();
   const workPackages = useAllWorkPackagesUpcomingDeadlines();
+
+  if (workPackages.isError) {
+    return <ErrorPage message={workPackages.error.message} error={workPackages.error} />;
+  }
+
+  const fullDisplay = (
+    <Row className="flex-nowrap overflow-auto">
+      {workPackages.data?.map((wp) => (
+        <Col className="px-1" xs={12} sm={7} md={5} lg={4} xl={3}>
+          <Card border={theme.cardBorder} bg={theme.cardBg}>
+            <Card.Body className="p-3">
+              <Card.Title className="mb-2">
+                <Link to={`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`}>
+                  {wbsPipe(wp.wbsNum)} - {wp.name}
+                </Link>
+              </Card.Title>
+              <Card.Text>
+                <Container fluid>
+                  <Row className="pb-1">End Date: {datePipe(wp.endDate)}</Row>
+                  <Row className="pb-1">Engineering Lead: {fullNamePipe(wp.projectLead)}</Row>
+                  <Row className="pb-1">Project Manager: {fullNamePipe(wp.projectManager)}</Row>
+                  <Row>
+                    {wp.expectedActivities.length} Expected Activities, {wp.deliverables.length}{' '}
+                    Deliverables
+                  </Row>
+                </Container>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+
   return (
     <PageBlock
       title={'Upcoming Deadlines'}
       headerRight={<></>}
       body={
-        <Container fluid>
-          <Row className="flex-nowrap overflow-auto">
-            {workPackages.data?.map((wp) => (
-              <Col className="px-1" sm={7} md={5} lg={4} xl={3}>
-                <Card border={theme.cardBorder} bg={theme.cardBg}>
-                  <Card.Body className="p-3">
-                    <Card.Title className="mb-2">
-                      <Link to={`${routes.PROJECTS}/${wbsPipe(wp.wbsNum)}`}>
-                        {wbsPipe(wp.wbsNum)} - {wp.name}
-                      </Link>
-                    </Card.Title>
-                    <Card.Text>
-                      <Container fluid>
-                        <Row className="pb-1">End Date: {datePipe(wp.endDate)}</Row>
-                        <Row className="pb-1">Engineering Lead: {fullNamePipe(wp.projectLead)}</Row>
-                        <Row className="pb-1">
-                          Project Manager: {fullNamePipe(wp.projectManager)}
-                        </Row>
-                        <Row>
-                          {wp.expectedActivities.length} Expected Activites,{' '}
-                          {wp.deliverables.length} Deliverables
-                        </Row>
-                      </Container>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        <Container fluid>{workPackages.isLoading ? <LoadingIndicator /> : fullDisplay}</Container>
       }
     />
   );

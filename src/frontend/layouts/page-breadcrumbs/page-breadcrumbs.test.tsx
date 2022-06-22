a4f5186211fc7a0161d60692ca888ec15186fb39
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-06-22 10:11:25 -0400

    #745 - enhance breadcrumb testing

--- src/frontend/layouts/page-breadcrumbs/page-breadcrumbs.test.tsx
+++ src/frontend/layouts/page-breadcrumbs/page-breadcrumbs.test.tsx
@@ -4,10 +4,12 @@
  */
 
 import { render, routerWrapperBuilder, screen } from '../../../test-support/test-utils';
+import { testLinkItems } from '../../../test-support/test-data/test-utils.stub';
+import { LinkItem } from '../../../shared/types';
 import PageBreadcrumbs from './page-breadcrumbs';
 
 // Render component under test
-const renderComponent = (title = 'test', pages = []) => {
+const renderComponent = (title = 'test', pages: LinkItem[] = []) => {
   const RouterWrapper = routerWrapperBuilder({});
   return render(
     <RouterWrapper>
@@ -21,9 +23,24 @@ describe('page-breadcrumbs component', () => {
     renderComponent();
   });
 
-  it('renders title', () => {
+  it('renders current page', () => {
     renderComponent();
 
     expect(screen.getByText('test')).toBeInTheDocument();
+  });
+
+  it('renders home by default', () => {
+    renderComponent();
+
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+
+  it('renders provided pages', () => {
+    renderComponent('hello there', testLinkItems);
+
+    expect(screen.getByText('hello there')).toBeInTheDocument();
+    expect(screen.getAllByText('Home').length).toEqual(2);
+    expect(screen.getByText('Projects')).toBeInTheDocument();
+    expect(screen.getByText('Change Requests')).toBeInTheDocument();
   });
 });

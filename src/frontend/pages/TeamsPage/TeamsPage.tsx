c120566fe98db94a73cc2d883b939761cbef0c73
Anthony Bernardi <bernardi.an@husky.neu.edu>
2022-06-27 14:19:15 -0400

    #732 - made teams page say under development

--- src/frontend/pages/TeamsPage/TeamsPage.tsx
+++ src/frontend/pages/TeamsPage/TeamsPage.tsx
@@ -3,64 +3,15 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Col, Container, Row } from 'react-bootstrap';
-import { faScroll, faCode, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
-import { iconLinkPipe } from '../../../shared/pipes';
-import PageTitle from '../../layouts/page-title/page-title';
-import PageBlock from '../../layouts/page-block/page-block';
+import styles from './TeamsPage.module.css';
 
 const TeamsPage: React.FC = () => {
   return (
-    <Container fluid>
-      <PageTitle title="Teams" />
-      <PageBlock
-        title="Resources"
-        headerRight={<></>}
-        body={
-          <Container fluid>
-            <Row className="pb-2">Check out these helpful resources:</Row>
-            <Row>
-              <Col md={4} lg={3}>
-                {iconLinkPipe(
-                  faScroll,
-                  'Glossary Document',
-                  'https://docs.google.com/document/d/1_kr7PQxjYKvBTmZc8cxeSv5xx0lE88v0wVXkVg3Mez8/edit?usp=sharing'
-                )}
-              </Col>
-              <Col>Got any suggestions for additional resources? Drop a message in Slack!</Col>
-            </Row>
-          </Container>
-        }
-      />
-      <PageBlock
-        title="Support"
-        headerRight={<></>}
-        body={
-          <Container fluid>
-            <Row className="pb-2">
-              Any and all questions, comments, suggestions, bugs, or other issues can be directed to
-              the resources below:
-            </Row>
-            <Row>
-              <Col sm={5} md={4} lg={3}>
-                {iconLinkPipe(
-                  faCommentAlt,
-                  'Message in Slack',
-                  'slack://channel?team=T7MHAQ5TL&id=C02U5TKHLER'
-                )}
-              </Col>
-              <Col sm={6} md={4} lg={3}>
-                {iconLinkPipe(
-                  faCode,
-                  'Submit a ticket on GitHub',
-                  'https://github.com/Northeastern-Electric-Racing/PM-Dashboard-v2/issues/new/choose'
-                )}
-              </Col>
-            </Row>
-          </Container>
-        }
-      />
-    </Container>
+    <div className={`pt-5 ${styles.page_not_found}`}>
+      <h1 className={styles.title}>Uh Oh</h1>
+      <h3 className={styles.subtitle}>This page is still under development</h3>
+      <h5 className={styles.subtitle}>Check back soon!</h5>
+    </div>
   );
 };
 

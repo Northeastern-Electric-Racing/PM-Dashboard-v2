ac440c1b07969e44e4c752c92b78bed8f070694a
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-07-02 02:30:04 -0700

    #730 - base update user settings form working

--- src/frontend/pages/SettingsPage/user-settings/user-settings.tsx
+++ src/frontend/pages/SettingsPage/user-settings/user-settings.tsx
@@ -4,29 +4,52 @@
  */
 
 import { useState } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { useSingleUserSettings } from '../../../../services/users.hooks';
+import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { ThemeName } from 'utils';
+import { useTheme } from '../../../../services/theme.hooks';
+import { useSingleUserSettings, useUpdateUserSettings } from '../../../../services/users.hooks';
 import LoadingIndicator from '../../../components/loading-indicator/loading-indicator';
 import PageBlock from '../../../layouts/page-block/page-block';
 import ErrorPage from '../../ErrorPage/error-page';
+import UserSettingsEdit from './user-settings-edit/user-settings-edit';
 import UserSettingsView from './user-settings-view/user-settings-view';
 
 interface UserSettingsProps {
   userId: number;
 }
 
+export interface FormInput {
+  defaultTheme: ThemeName;
+}
+
 const UserSettings: React.FC<UserSettingsProps> = ({ userId }) => {
   const [edit, setEdit] = useState(false);
   const userSettings = useSingleUserSettings(userId);
+  const update = useUpdateUserSettings();
+  const theme = useTheme();
 
-  if (userSettings.isLoading) return <LoadingIndicator />;
-  if (userSettings.isError) {
+  if (userSettings.isLoading || update.isLoading) return <LoadingIndicator />;
+  if (userSettings.isError)
     return <ErrorPage error={userSettings.error} message={userSettings.error.message} />;
-  }
+  if (update.isError) return <ErrorPage error={update.error!} message={update.error?.message!} />;
+
+  const handleConfirm = async ({ defaultTheme }: FormInput) => {
+    setEdit(false);
+    await update.mutateAsync({ id: userSettings.data?.id!, defaultTheme });
+    const res = await userSettings.refetch();
+
+    if (res.data?.defaultTheme && res.data?.defaultTheme !== theme.name) {
+      theme.toggleTheme!(res.data?.defaultTheme);
+    }
+  };
 
   const editButton = <Button onClick={() => setEdit(true)}>Edit</Button>;
   const saveButton = (
     <Button
+      variant="success"
+      type="submit"
+      form="update-user-settings"
       onClick={() => {
         setEdit(false);
       }}
@@ -35,23 +58,31 @@ const UserSettings: React.FC<UserSettingsProps> = ({ userId }) => {
     </Button>
   );
   const cancelButton = (
-    <Button className="mr-1" variant="secondary" onClick={() => setEdit(false)}>
+    <Button
+      className="mr-1"
+      variant="secondary"
+      form="update-user-settings"
+      onClick={() => setEdit(false)}
+    >
       Cancel
     </Button>
   );
-  const buttonContainer = (
-    <div className="d-flex flex-row">
-      {!edit ? editButton : ''}
-      {edit ? cancelButton : ''}
-      {edit ? saveButton : ''}
-    </div>
-  );
 
   return (
-    <PageBlock title="User Settings" headerRight={buttonContainer}>
-      <Container fluid>
+    <PageBlock
+      title="User Settings"
+      headerRight={
+        <div className="d-flex flex-row">
+          {!edit ? editButton : cancelButton}
+          {edit ? saveButton : ''}
+        </div>
+      }
+    >
+      {!edit ? (
         <UserSettingsView settings={userSettings.data!} />
-      </Container>
+      ) : (
+        <UserSettingsEdit currentSettings={userSettings.data!} onSubmit={handleConfirm} />
+      )}
     </PageBlock>
   );
 };

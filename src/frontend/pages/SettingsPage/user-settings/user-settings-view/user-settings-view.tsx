b4f02d808fea6e422de8c4617900b1ef5a818624
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-07-02 02:12:03 -0700

    #730 - move container into user settings view

--- src/frontend/pages/SettingsPage/user-settings/user-settings-view/user-settings-view.tsx
+++ src/frontend/pages/SettingsPage/user-settings/user-settings-view/user-settings-view.tsx
@@ -3,20 +3,23 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { UserSettings } from 'utils';
 
 interface UserSettingsViewProps {
   settings: UserSettings;
 }
 
+/** Component to display user settings */
 const UserSettingsView: React.FC<UserSettingsViewProps> = ({ settings }) => {
   return (
-    <Row>
-      <Col md={6} lg={4}>
-        <b>Default Theme:</b> {settings.defaultTheme}
-      </Col>
-    </Row>
+    <Container fluid>
+      <Row>
+        <Col md={6} lg={4}>
+          <b>Default Theme:</b> {settings.defaultTheme}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 

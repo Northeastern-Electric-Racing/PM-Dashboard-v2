f183f339373f0d2a3ed072dfd2fb042dead656a6
James C-D <51571627+jamescd18@users.noreply.github.com>
2022-07-14 19:02:24 -0400

    #730 - remove theme toggle in top bar

--- src/frontend/layouts/nav-top-bar/nav-top-bar.tsx
+++ src/frontend/layouts/nav-top-bar/nav-top-bar.tsx
@@ -3,20 +3,17 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
-import { Dropdown, Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { routes } from '../../../shared/routes';
 import { useAuth } from '../../../services/auth.hooks';
 import { fullNamePipe } from '../../../shared/pipes';
-import { useTheme } from '../../../services/theme.hooks';
-import themes from '../../../shared/themes';
 import NavUserMenu from './nav-user-menu/nav-user-menu';
 import NavNotificationsMenu from './nav-notifications-menu/nav-notifications-menu';
 import styles from './nav-top-bar.module.css';
 
 const NavTopBar: React.FC = () => {
   const auth = useAuth();
-  const theme = useTheme();
 
   return (
     <Navbar className={styles.mainBackground} variant="light" expand="md" fixed="top">
@@ -33,18 +30,6 @@ const NavTopBar: React.FC = () => {
       <Navbar.Toggle aria-controls="nav-top-bar-items" />
       <Navbar.Collapse id="nav-top-bar-items">
         <Nav className="ml-auto">
-          <Dropdown className={styles.dropdown}>
-            <Dropdown.Toggle variant={theme.cardBg}>{theme.name}</Dropdown.Toggle>
-            <Dropdown.Menu>
-              {themes
-                .filter((t) => t.name !== theme.name)
-                .map((t) => (
-                  <Dropdown.Item key={t.name} onClick={() => theme.toggleTheme!(t.name)}>
-                    {t.name}
-                  </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-          </Dropdown>
           <NavNotificationsMenu />
           <div className={styles.username}>{fullNamePipe(auth.user)}</div>
           <NavUserMenu />

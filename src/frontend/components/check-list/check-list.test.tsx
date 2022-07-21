0096ff0dfdccaaa277b7b9464eab9f1e24153180
Zack Roth <zack27r@gmail.com>
2022-07-21 15:39:32 -0400

    #696 - Added tests

--- src/frontend/components/check-list/check-list.test.tsx
+++ src/frontend/components/check-list/check-list.test.tsx
@@ -3,4 +3,54 @@
  * See the LICENSE file in the repository root folder for details.
  */
 
+import { render, screen } from '@testing-library/react';
+import { useTheme } from '../../../services/theme.hooks';
+import themes from '../../../shared/themes';
+import { Theme } from '../../../shared/types';
 import CheckList from './check-list';
+
+jest.mock('../../../services/theme.hooks');
+const mockTheme = useTheme as jest.Mock<Theme>;
+
+const mockHook = () => {
+  mockTheme.mockReturnValue(themes[0]);
+};
+
+describe('Check List Component', () => {
+  const testList = [
+    { details: 'Check #1', resolved: false },
+    { details: 'Check #2', resolved: true },
+    { details: 'Check #3', resolved: false }
+  ];
+
+  beforeEach(() => mockHook());
+
+  it('renders the component title', () => {
+    render(<CheckList title={'test'} list={[]} />);
+
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+
+  it('renders all details', () => {
+    render(<CheckList title={'test'} list={testList} />);
+
+    expect(screen.getByText('Check #1')).toBeInTheDocument();
+    expect(screen.getByText('Check #2')).toBeInTheDocument();
+    expect(screen.getByText('Check #3')).toBeInTheDocument();
+  });
+
+  it('renders all buttons"', () => {
+    render(<CheckList title={'test'} list={testList} />);
+
+    //expect(screen.getByText('Convert to CR')).toBeInTheDocument();
+    //not sure why thats failing
+    expect(screen.getByText('Delete')).toBeInTheDocument();
+    expect(screen.getByText('+')).toBeInTheDocument();
+  });
+
+  it('renders the header right', () => {
+    render(<CheckList title="test" headerRight="testheaderright" list={[]} />);
+
+    expect(screen.getByText('testheaderright')).toBeInTheDocument();
+  });
+});
